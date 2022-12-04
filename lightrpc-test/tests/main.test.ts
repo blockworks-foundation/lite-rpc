a3207369edbd2d373b78d3da4ebc04f8718408b5
Lioncat2002 <sankhayan2002@gmail.com>
2022-12-04 12:35:04 +0530

    feat: working on ts tests

--- lightrpc-test/tests/main.test.ts
+++ lightrpc-test/tests/main.test.ts
@@ -6,26 +6,26 @@ test('send and confirm transaction', async () => {
   const payer = Keypair.generate();
 
   await connection.requestAirdrop(payer.publicKey, LAMPORTS_PER_SOL);
-  //const recentBlockhash = (await connection.getLatestBlockhash('confirmed')).blockhash;
-  //const versionedTx = new VersionedTransaction(
-  //  new Message({
-  //    header: {
-  //      numRequiredSignatures: 1,
-  //      numReadonlySignedAccounts: 0,
-  //      numReadonlyUnsignedAccounts: 0,
-  //    },
-  //    recentBlockhash,
-  //    instructions: [],
-  //    accountKeys: [payer.publicKey.toBase58()],
-  //  }),
-  //);
-//
-  //versionedTx.sign([payer]);
-  //const signature = await connection.sendTransaction(versionedTx);
-  //const latestBlockHash = await connection.getLatestBlockhash();
-  //await connection.confirmTransaction({
-  //  blockhash: latestBlockHash.blockhash,
-  //  lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
-  //  signature: signature,
-  //});
+  const recentBlockhash = (await connection.getLatestBlockhash('confirmed')).blockhash;
+  const versionedTx = new VersionedTransaction(
+    new Message({
+      header: {
+        numRequiredSignatures: 1,
+        numReadonlySignedAccounts: 0,
+        numReadonlyUnsignedAccounts: 0,
+      },
+      recentBlockhash,
+      instructions: [],
+      accountKeys: [payer.publicKey.toBase58()],
+    }),
+  );
+
+  versionedTx.sign([payer]);
+  const signature = await connection.sendTransaction(versionedTx);
+  const latestBlockHash = await connection.getLatestBlockhash();
+  await connection.confirmTransaction({
+    blockhash: latestBlockHash.blockhash,
+    lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+    signature: signature,
+  });
 });
