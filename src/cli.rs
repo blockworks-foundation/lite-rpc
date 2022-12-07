51fe807e4a30950abd8f3b23bd33c4ec0a0cd4ee
Lioncat2002 <sankhayan2002@gmail.com>
2022-12-07 20:35:18 +0530

    feat: rewritten the cli to support extra commands

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,6 @@
-use {clap::Parser, solana_cli_config::ConfigInput};
+use clap::Subcommand;
+
+use clap::Parser;
 
 /// Holds the configuration for a single run of the benchmark
 #[derive(Parser, Debug)]
@@ -11,6 +13,11 @@ use {clap::Parser, solana_cli_config::ConfigInput};
     "
 )]
 pub struct Args {
+
+    #[clap(subcommand)]
+    pub command:Command,
+
+    /*
     #[arg(short, long, default_value_t = String::from("8899"))]
     pub port: String,
     #[arg(short, long, default_value_t = String::from("8900"))]
@@ -19,10 +26,13 @@ pub struct Args {
     pub rpc_url: String,
     #[arg(short, long,  default_value_t = String::new())]
     pub websocket_url: String,
+    */
 }
-
+/* 
 impl Args {
+
     pub fn resolve_address(&mut self) {
+        
         if self.rpc_url.is_empty() {
             let (_, rpc_url) = ConfigInput::compute_json_rpc_url_setting(
                 self.rpc_url.as_str(),
@@ -40,4 +50,20 @@ impl Args {
             self.websocket_url = ws_url;
         }
     }
+    
+}
+*/
+#[derive(Subcommand,Debug)]
+pub enum Command {
+    Run {
+        #[arg(short, long, default_value_t = String::from("8899"))]
+        port: String,
+        #[arg(short, long, default_value_t = String::from("8900"))]
+        subscription_port: String,
+        #[arg(short, long, default_value_t = String::from("http://localhost:8899"))]
+        rpc_url: String,
+        #[arg(short, long,  default_value_t = String::new())]
+        websocket_url: String,
+    },
+    Test,
 }
