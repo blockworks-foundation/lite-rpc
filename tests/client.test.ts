83e2fc9d1251983c5dc1ac5c5e9b9e6d98622ef5
Aniket Prajapati <contact@aniketprajapati.me>
2023-01-04 19:16:43 +0530

    typescript tests

---
+++ tests/client.test.ts
@@ -0,0 +1,25 @@
+import { Connection, Keypair, LAMPORTS_PER_SOL, SystemProgram, sendAndConfirmTransaction, Transaction, PublicKey } from "@solana/web3.js";
+
+jest.setTimeout(60000);
+
+test('send and confirm transaction', async () => {
+    const connection = new Connection('http://127.0.0.1:8890', 'confirmed');
+    const payer = Keypair.generate();
+    const toAccount = Keypair.generate().publicKey;
+
+    const airdropSignature = await connection.requestAirdrop(payer.publicKey, LAMPORTS_PER_SOL * 2);
+    await connection.confirmTransaction(airdropSignature, 'finalized');
+
+    const transaction = new Transaction();
+
+    // Add an instruction to execute
+    transaction.add(
+        SystemProgram.transfer({
+            fromPubkey: payer.publicKey,
+            toPubkey: toAccount,
+            lamports: LAMPORTS_PER_SOL,
+        }),
+    );
+
+    await sendAndConfirmTransaction(connection, transaction, [payer]);
+});
