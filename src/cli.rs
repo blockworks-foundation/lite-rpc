35a9e2e193fb5165bc501ea66808b067d872f46c
Aniket Prajapati <contact@aniketprajapati.me>
2023-01-08 22:28:14 +0530

    -l flag

--- src/cli.rs
+++ src/cli.rs
@@ -8,7 +8,7 @@ pub struct Args {
     pub rpc_addr: String,
     #[arg(short, long, default_value_t = String::from(DEFAULT_WS_ADDR))]
     pub ws_addr: String,
-    #[arg(long, default_value_t = String::from("127.0.0.1:8890"))]
+    #[arg(short='l',long, default_value_t = String::from("127.0.0.1:8890"))]
     pub lite_rpc_http_addr: String,
     #[arg(short='s', long, default_value_t = String::from("127.0.0.1:8891"))]
     pub lite_rpc_ws_addr: String,
