2faa23b9ad9d5f6829c24179c68520567726a810
Aniket Prajapati <contact@aniketprajapati.me>
2023-01-10 20:30:21 +0530

    fix and fmt

--- src/workers/mod.rs
+++ src/workers/mod.rs
@@ -1,9 +1,9 @@
 mod block_listenser;
-mod tx_sender;
 mod cleaner;
 mod metrics;
+mod tx_sender;
 
 pub use block_listenser::*;
-pub use tx_sender::*;
 pub use cleaner::*;
 pub use metrics::*;
+pub use tx_sender::*;
