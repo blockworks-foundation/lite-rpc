6ab77f98061f86a49d9ac383abf7163a9b74e61b
aniketfuryrocks <contact@aniketprajapati.me>
2023-01-23 21:18:12 +0530

    connect to postgres

--- src/workers/metrics_postgres.rs
+++ src/workers/metrics_postgres.rs
@@ -1,9 +1,11 @@
-use anyhow::bail;
-use postgres_openssl::MakeTlsConnector;
+use anyhow::{bail, Context};
+use log::info;
+use postgres_native_tls::MakeTlsConnector;
+use tokio::fs;
 use tokio::task::JoinHandle;
 use tokio_postgres::Client;
 
-use openssl::ssl::{SslConnector, SslMethod};
+use native_tls::{Certificate, Identity, TlsConnector};
 
 use super::{Metrics, MetricsCapture};
 
@@ -18,11 +20,18 @@ impl MetricsPostgres {
         metrics_capture: MetricsCapture,
         porstgres_config: &str,
     ) -> anyhow::Result<Self> {
-        let mut builder = SslConnector::builder(SslMethod::tls()).unwrap();
-        builder.set_ca_file("ca.pem")?;
-//        builder.set_private_key("client-key.pem")?;
+        let connector = TlsConnector::builder()
+            .add_root_certificate(Certificate::from_pem(&fs::read("ca.pem").await?)?)
+            .identity(
+                Identity::from_pkcs12(&fs::read("client.pks").await?, "p").context("Identity")?,
+            )
+            .danger_accept_invalid_hostnames(true)
+            .danger_accept_invalid_certs(true)
+            .build()?;
 
-        let connector = MakeTlsConnector::new(builder.build());
+        info!("making tls config");
+
+        let connector = MakeTlsConnector::new(connector);
         let (client, connection) = tokio_postgres::connect(porstgres_config, connector).await?;
 
         Ok(Self {
@@ -41,6 +50,8 @@ impl MetricsPostgres {
         } = self;
 
         let metrics_send: JoinHandle<anyhow::Result<()>> = tokio::spawn(async move {
+            info!("Sending Metrics To Postgres");
+
             loop {
                 let Metrics {
                     txs_sent,
@@ -53,14 +64,14 @@ impl MetricsPostgres {
                 } = metrics_capture.get_metrics().await;
 
                 client.execute(
-                    r#"INSERT INTO Metrics 
-                        (txs_sent, txs_confirmed, txs_finalized, transactions_per_second,  confirmations_per_second,  finalized_per_second,  memory_used)
+                        r#"INSERT INTO LiteRpcMetrics
+                        (txs_sent, txs_confirmed, txs_finalized, transactions_per_second, confirmations_per_second, finalized_per_second, memory_used)
                         VALUES 
-                        ($1, $2, $3, $4, $5, $6)
+                        ($1, $2, $3, $4, $5, $6, $7)
                     "#,
-                    &[&(txs_sent as u32), &(txs_confirmed as u32), &(txs_finalized as u32), &(txs_ps as u32), &(txs_confirmed_ps as u32), &(txs_finalized_ps as u32), &(mem_used.unwrap_or_default() as u32)],
+                    &[&(txs_sent as i64), &(txs_confirmed as i64), &(txs_finalized as i64), &(txs_ps as i64), &(txs_confirmed_ps as i64), &(txs_finalized_ps as i64), &(mem_used.unwrap_or_default() as i64)],
                 )
-                .await?;
+                .await.unwrap();
 
                 one_second.tick().await;
             }
