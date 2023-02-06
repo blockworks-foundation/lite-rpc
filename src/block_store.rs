80d4e06147ed6fcd8ca4691af6bd704a26e71944
aniketfuryrocks <contact@aniketprajapati.me>
2023-02-06 20:37:39 +0530

    add MAX_RECENT_BLOCKHASSHES and some logs

--- src/block_store.rs
+++ src/block_store.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 
 use dashmap::DashMap;
 
+use log::info;
 use solana_rpc_client::nonblocking::rpc_client::RpcClient;
 use solana_sdk::commitment_config::CommitmentConfig;
 use tokio::sync::RwLock;
@@ -92,6 +93,7 @@ impl BlockStore {
         block_info: BlockInformation,
         commitment_config: CommitmentConfig,
     ) {
+        info!("ab {blockhash} {block_info:?}");
         // Write to block store first in order to prevent
         // any race condition i.e prevent some one to
         // ask the map what it doesn't have rn
