b95e63df66efbc7e06740db5bdf115eec6cdc71b
Godmode Galactus <godmodegalactus@gmail.com>
2023-03-17 10:57:15 +0100

    Making the default fanout again to 32

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,7 @@ pub const DEFAULT_TX_MAX_RETRIES: u16 = 1;
 #[from_env]
 pub const DEFAULT_TX_BATCH_SIZE: usize = 512;
 #[from_env]
-pub const DEFAULT_FANOUT_SIZE: u64 = 12;
+pub const DEFAULT_FANOUT_SIZE: u64 = 32;
 #[from_env]
 pub const DEFAULT_TX_BATCH_INTERVAL_MS: u64 = 1;
 #[from_env]
