51b15a844e4ea751e704ca10437f0e12bb4d08e2
Godmode Galactus <godmodegalactus@gmail.com>
2023-08-02 14:45:36 +0200

    changes in validator_identity

--- quic-forward-proxy/src/validator_identity.rs
+++ quic-forward-proxy/src/validator_identity.rs
@@ -1,45 +1,33 @@
 use solana_sdk::pubkey::Pubkey;
 use solana_sdk::signature::Keypair;
-use solana_sdk::signer::Signer;
+use solana_sdk::signer::{Signer};
 use std::fmt::Display;
 use std::sync::Arc;
 
 #[derive(Clone)]
 pub struct ValidatorIdentity {
-    keypair: Option<Arc<Keypair>>,
-    dummy_keypair: Arc<Keypair>,
+    keypair: Arc<Keypair>,
 }
 
 impl ValidatorIdentity {
     pub fn new(keypair: Option<Keypair>) -> Self {
-        let dummy_keypair = Keypair::new();
+        let keypair = keypair.unwrap();
         ValidatorIdentity {
-            keypair: keypair.map(|kp| Arc::new(kp)),
-            dummy_keypair: Arc::new(dummy_keypair),
+            keypair: Arc::new(keypair),
         }
     }
 
     pub fn get_keypair_for_tls(&self) -> Arc<Keypair> {
-        match &self.keypair {
-            Some(keypair) => keypair.clone(),
-            None => self.dummy_keypair.clone(),
-        }
+        self.keypair.clone()
     }
 
     pub fn get_pubkey(&self) -> Pubkey {
-        let keypair = match &self.keypair {
-            Some(keypair) => keypair.clone(),
-            None => self.dummy_keypair.clone(),
-        };
-        keypair.pubkey()
+        self.keypair.pubkey()
     }
 }
 
 impl Display for ValidatorIdentity {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match &self.keypair {
-            Some(keypair) => write!(f, "{}", keypair.pubkey().to_string()),
-            None => write!(f, "no keypair"),
-        }
+        write!(f, "{}", self.keypair.pubkey().to_string())
     }
 }
