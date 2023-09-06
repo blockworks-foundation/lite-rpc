5087df9466b7ed5fea1ad722b0b860386dbb042b
GroovieGermanikus <groovie@mango.markets>
2023-09-06 18:30:47 +0200

    add examples for hostname:port

--- quic-forward-proxy/src/cli.rs
+++ quic-forward-proxy/src/cli.rs
@@ -5,6 +5,7 @@ use clap::Parser;
 pub struct Args {
     #[arg(short = 'k', long, default_value_t = String::new())]
     pub identity_keypair: String,
+    // e.g. 0.0.0.0:11111 or "localhost:11111"
     #[arg(short = 'l', long)]
     pub proxy_listen_addr: String,
 }
