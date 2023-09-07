d16bd5bd0d784ff4194bea19bca0c2c3365947b3
Godmode Galactus <godmodegalactus@gmail.com>
2023-09-07 16:46:14 +0200

    RPCTester should test rpc and not self

--- lite-rpc/src/rpc_tester.rs
+++ lite-rpc/src/rpc_tester.rs
@@ -1,25 +1,20 @@
-use std::net::SocketAddr;
-
 use anyhow::bail;
-use lite_rpc::cli::Args;
 use prometheus::{opts, register_gauge, Gauge};
 use solana_rpc_client::nonblocking::rpc_client::RpcClient;
+use std::sync::Arc;
 
 lazy_static::lazy_static! {
     static ref RPC_RESPONDING: Gauge =
     register_gauge!(opts!("literpc_rpc_responding", "If LiteRpc is responding")).unwrap();
 }
 
-pub struct RpcTester(RpcClient);
+pub struct RpcTester {
+    rpc_client: Arc<RpcClient>,
+}
 
-impl From<&Args> for RpcTester {
-    fn from(value: &Args) -> Self {
-        let addr: SocketAddr = value
-            .lite_rpc_http_addr
-            .parse()
-            .expect("Invalid literpc http address");
-
-        RpcTester(RpcClient::new(format!("http://0.0.0.0:{}", addr.port())))
+impl RpcTester {
+    pub fn new(rpc_client: Arc<RpcClient>) -> Self {
+        Self { rpc_client }
     }
 }
 
@@ -27,11 +22,12 @@ impl RpcTester {
     /// Starts a loop that checks if the rpc is responding every 5 seconds
     pub async fn start(self) -> anyhow::Result<()> {
         let mut error_counter = 0;
+        let rpc_client = self.rpc_client;
         loop {
             // sleep for 5 seconds
             tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
             // do a simple request to self for getVersion
-            let Err(err) = self.0.get_version().await else {
+            let Err(err) = rpc_client.get_version().await else {
                 RPC_RESPONDING.set(1.0);
                 continue;
             };
