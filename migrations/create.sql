6b8bdecfbd85d0bc342021064da167dbd4ef5f2a
GroovieGermanikus <groovie@mango.markets>
2023-11-06 21:22:00 +0100

    use postgres "TEXT" instead of char/varchar
    note: the types are compatible and require no migration

--- migrations/create.sql
+++ migrations/create.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA lite_rpc;
 
 CREATE TABLE lite_rpc.Txs (
   id SERIAL NOT NULL PRIMARY KEY,
-  signature CHAR(88) NOT NULL,
+  signature TEXT NOT NULL,
   recent_slot BIGINT NOT NULL,
   forwarded_slot BIGINT NOT NULL,
   forwarded_local_time TIMESTAMP WITH TIME ZONE NOT NULL,
@@ -24,5 +24,5 @@ CREATE TABLE lite_rpc.Blocks (
 
 CREATE TABLE lite_rpc.AccountAddrs (
   id SERIAL PRIMARY KEY,
-  addr VARCHAR(45) NOT NULL
+  addr TEXT NOT NULL
 );
