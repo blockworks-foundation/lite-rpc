7d4c97a4a3071d768c6834733a3d7889a017c773
GroovieGermanikus <groovie@mango.markets>
2023-11-17 15:55:45 +0100

    debug confirmation-finalized state

--- lite-rpc/tests/storage_integration_tests.rs
+++ lite-rpc/tests/storage_integration_tests.rs
@@ -1,3 +1,5 @@
+use std::cell::OnceCell;
+use std::collections::{HashMap, HashSet};
 use jsonrpsee::tracing::warn;
 use log::{debug, error, info};
 use solana_lite_rpc_cluster_endpoints::endpoint_stremers::EndpointStreaming;
@@ -10,11 +12,13 @@ use solana_lite_rpc_history::block_stores::postgres_block_store::PostgresBlockSt
 use solana_rpc_client::nonblocking::rpc_client::RpcClient;
 use std::sync::Arc;
 use std::time::{Duration, Instant};
+use solana_sdk::clock::Slot;
 use solana_sdk::commitment_config::CommitmentConfig;
 use tokio::sync::broadcast::error::RecvError;
 use tokio::task::JoinHandle;
 use tokio::time::sleep;
 use tracing_subscriber::EnvFilter;
+use tracing_subscriber::fmt::format;
 
 #[tokio::test]
 async fn storage_test() {
@@ -44,7 +48,7 @@ async fn storage_test() {
     drop(blocks_notifier);
 
     info!("Run tests for some time ...");
-    sleep(Duration::from_secs(10)).await;
+    sleep(Duration::from_secs(20)).await;
 
     jh1.abort();
     jh2.abort();
@@ -68,6 +72,8 @@ fn storage_listen(
                         block.transactions.len()
                     );
 
+                    // TODO we should intercept finalized blocks and try to update only the status optimistically
+
                     let produced_block = ProducedBlock {
                         transactions: block.transactions,
                         leader_id: block.leader_id,
@@ -85,6 +91,7 @@ fn storage_listen(
                     // avoid backpressure here!
                     // TODO check timing
                     block_storage.save(&produced_block).await.unwrap();
+                    // we should be faster than 150ms here
                     debug!(
                         "Saving block to postgres took {:.2}ms",
                         started.elapsed().as_secs_f64() * 1000.0
@@ -106,10 +113,23 @@ fn storage_listen(
     })
 }
 
+#[derive(Debug, Clone)]
+struct BlockDebugDetails {
+    pub blockhash: String,
+    pub block: ProducedBlock,
+}
+
 fn block_debug_listen(block_notifier: BlockStream) -> JoinHandle<()> {
     tokio::spawn(async move {
         let mut last_highest_slot_number = 0;
         let mut block_notifier = block_notifier;
+
+        let mut confirmed_blocks_by_slot = HashMap::<Slot, BlockDebugDetails>::new();
+        let mut finalized_blocks = HashSet::<Slot>::new();
+
+        let mut warmup = true;
+        let mut warmup_first_confirmed: Slot = 0;
+
         loop {
             match block_notifier.recv().await {
                 Ok(block) => {
@@ -118,6 +138,67 @@ fn block_debug_listen(block_notifier: BlockStream) -> JoinHandle<()> {
                         block.slot,
                         block.transactions.len()
                     );
+
+                    /// check the commitment transition from confirmed to finalized
+                    if warmup {
+                        debug!("Warming up {} ...", block.slot);
+
+                        if block.commitment_config == CommitmentConfig::confirmed() && warmup_first_confirmed == 0 {
+                            warmup_first_confirmed = block.slot;
+                        }
+
+                        if block.commitment_config == CommitmentConfig::finalized() {
+                            if block.slot >= warmup_first_confirmed {
+                                warmup = false;
+                                debug!("Warming done (slot {})", block.slot);
+
+                            }
+                        }
+
+                    } else {
+
+                        if block.commitment_config == CommitmentConfig::confirmed() {
+                            let prev_block = confirmed_blocks_by_slot.insert(block.slot, BlockDebugDetails {
+                                blockhash: block.blockhash.clone(),
+                                block: block.clone(),
+                            });
+                            assert!(prev_block.is_none(), "Must not see a confirmed block twice");
+                        } else if block.commitment_config == CommitmentConfig::finalized() {
+                            let finalized_block = &block;
+                            let finalized_block_existed = finalized_blocks.insert(finalized_block.slot);
+                            assert!(!finalized_block_existed, "Finalized block must have been seen before");
+                            let prev_block = confirmed_blocks_by_slot.get(&block.slot);
+                            match prev_block {
+                                Some(prev_block) => {
+                                    info!("Got finalized block {} with blockhash {} - prev confirmed was {}",
+                                        finalized_block.slot, finalized_block.blockhash, prev_block.blockhash);
+                                    // TODO is that correct?
+                                    assert_eq!(finalized_block.blockhash, prev_block.blockhash, "Must see the same blockhash for confirmed and finalized block");
+
+                                    debug!("confirmed: {:?}", to_string_without_transactions(&prev_block.block));
+                                    debug!("finalized: {:?}", to_string_without_transactions(&finalized_block));
+
+                                    assert_eq!(
+                                        to_string_without_transactions(&prev_block.block).replace("commitment_config=confirmed", "commitment_config=IGNORE"),
+                                        to_string_without_transactions(&finalized_block).replace("commitment_config=finalized", "commitment_config=IGNORE"),
+                                        "block tostring mismatch"
+                                    )
+
+                                }
+                                None => {
+                                    // note at startup we might see some orphan finalized blocks before we see matching pairs of confirmed-finalized blocks
+                                    panic!("Must see a confirmed block before it is finalized");
+                                }
+                            }
+                            if prev_block.is_none() {
+                                // note at startup we might see some finalized blocks before we see the confirmed blocks
+                                warn!("Must see a confirmed block before it is finalized");
+                            } else {
+                            }
+                        }
+                    } // -- non-warmup
+
+                    /// -- done
 
                     // check monotony
                     // note: this succeeds if poll_block parallelism is 1 (see NUM_PARALLEL_BLOCKS)
@@ -148,6 +229,31 @@ fn block_debug_listen(block_notifier: BlockStream) -> JoinHandle<()> {
             // ...
         }
     })
+}
+
+fn to_string_without_transactions(produced_block: &ProducedBlock) -> String {
+    format!(
+        r#"
+            leader_id={:?}
+            blockhash={}
+            block_height={}
+            slot={}
+            parent_slot={}
+            block_time={}
+            commitment_config={}
+            previous_blockhash={}
+        "#,
+        produced_block.leader_id,
+        produced_block.blockhash,
+        produced_block.block_height,
+        produced_block.slot,
+        produced_block.parent_slot,
+        produced_block.block_time,
+        produced_block.commitment_config.commitment,
+        produced_block.previous_blockhash,
+        // rewards
+        // transactions
+    )
 }
 
 fn configure_panic_hook() {
