4e62e8549c4ce61330f7dee52dcfcf517912bc31
GroovieGermanikus <groovie@mango.markets>
2023-11-17 16:33:47 +0100

    iterutils

---
+++ core/src/iterutils.rs
@@ -0,0 +1,19 @@
+
+pub enum Uniqueness {
+    ExactlyOne,
+    Multiple(usize),
+    Empty
+}
+
+impl Uniqueness {
+    pub fn inspect_len(len: usize) -> Uniqueness {
+        if len == 1 {
+            Uniqueness::ExactlyOne
+        } else if len == 0 {
+            Uniqueness::Empty
+        } else {
+            Uniqueness::Multiple(len)
+        }
+    }
+}
+
