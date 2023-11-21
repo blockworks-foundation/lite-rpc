5cc7cb9326c4397fb510c7a46024e3e0f67b9077
GroovieGermanikus <groovie@mango.markets>
2023-11-21 10:46:23 +0100

    bench postgres

--- history/examples/bench_postgres.rs
+++ history/examples/bench_postgres.rs
@@ -10,12 +10,12 @@ use log::info;
 use solana_sdk::blake3::Hash;
 use solana_sdk::signature::Signature;
 use tokio::time::Instant;
-use tokio_postgres::{Client, CopyInSink};
+use tokio_postgres::{Client, CopyInSink, GenericClient};
 use tokio_postgres::binary_copy::BinaryCopyInWriter;
 use tokio_postgres::types::{ToSql, Type};
 use solana_lite_rpc_history::postgres::postgres_session::PostgresSession;
 
-pub async fn copy_in(client: Arc<tokio_postgres::Client>) -> anyhow::Result<()> {
+pub async fn copy_in(client: &tokio_postgres::Client) -> anyhow::Result<()> {
 
     let statement = format!(
         r#"
@@ -52,7 +52,7 @@ pub async fn copy_in(client: Arc<tokio_postgres::Client>) -> anyhow::Result<()> 
     let writer = BinaryCopyInWriter::new(sink, &[Type::TEXT, Type::INT8, Type::TEXT, Type::INT8, Type::INT8, Type::INT8, Type::TEXT, Type::TEXT]);
     pin_mut!(writer);
 
-    const count: usize = 10000;
+    const count: usize = 100000;
     for i in 0..count {
         let slot_x = slot + i as i64;
         writer.as_mut().write(&[&sig, &slot_x, &err, &cu_requested, &prioritization_fees, &cu_consumed, &recent_blockhash, &message]).await.unwrap();
@@ -73,19 +73,22 @@ pub async fn main() {
     tracing_subscriber::fmt::init();
     env::set_var("PG_CONFIG", "host=localhost port=5433 dbname=stefan user=postgres sslmode=disable");
 
-    let write_session_single = PostgresSession::new_from_env().await.unwrap();
+    let dummy_session = PostgresSession::new_from_env().await.unwrap();
     // let write_session1 = PostgresSession::new_from_env().await.unwrap();
     // let write_session2 = PostgresSession::new_from_env().await.unwrap();
     // let write_session3 = PostgresSession::new_from_env().await.unwrap();
     // let write_session4 = PostgresSession::new_from_env().await.unwrap();
     // let write_session5 = PostgresSession::new_from_env().await.unwrap();
 
+    let row_count_before = count_rows(dummy_session.client.clone()).await;
+
+    let started = Instant::now();
+
     let mut write_sessions = Vec::new();
     for _i in 0..5 {
         write_sessions.push(PostgresSession::new_from_env().await.unwrap());
     }
 
-    let started = Instant::now();
     // let (ret1, ret2, ret3, ret4, ret5) = futures_util::join!(
     //     copy_in(&write_session1.client),
     //     copy_in(&write_session2.client),
@@ -93,27 +96,28 @@ pub async fn main() {
     //     copy_in(&write_session4.client),
     //     copy_in(&write_session5.client),
     // );
-    let queries = write_sessions.iter().map(|x| copy_in(x.client.clone())).collect_vec();
+    let queries = write_sessions.iter().map(|x| copy_in(&x.client)).collect_vec();
     let all_results: Vec<anyhow::Result<()>> = join_all(queries).await;
 
-    // for result in all_results {
-    //     result.unwrap();
-    // }
+    for result in all_results {
+        result.unwrap();
+    }
 
     info!("parallel write rows in {:02}ms", started.elapsed().as_secs_f64() * 1000.0);
 
-    let dummy_session = PostgresSession::new_from_env().await.unwrap();
-    let rows = dummy_session.client
-        .query("SELECT signature, slot, err, cu_requested, prioritization_fees, cu_consumed, recent_blockhash, message FROM public.transactions_copyin", &[])
+    let row_count_after = count_rows(dummy_session.client.clone()).await;
+    info!("total: {}", row_count_after);
+    info!("inserted: {}", row_count_after - row_count_before);
+
+}
+
+
+async fn count_rows(client: Arc<tokio_postgres::Client>) -> i64 {
+    let row = client
+        .query_one("SELECT count(*) FROM public.transactions_copyin", &[])
         .await
         .unwrap();
 
-    info!("total: {}", rows.len());
-
-    for (i, row) in rows.iter().take(10).enumerate() {
-        let sig = row.get::<_, &str>(0);
-        let slot = row.get::<_, i64>(1);
-        info!("row #{}: {} {}", i, sig, slot);
-    }
-
+    let count = row.get::<_, i64>(0);
+    count
 }
