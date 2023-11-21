fc49127bf2213c85e65715bb0afbdf62d8eea22b
GroovieGermanikus <groovie@mango.markets>
2023-11-21 16:28:37 +0100

    minor log fix

--- history/src/postgres/postgres_block.rs
+++ history/src/postgres/postgres_block.rs
@@ -196,7 +196,7 @@ impl PostgresBlock {
         }
 
         debug!(
-            "Inserting block {} row to schema {} postgres took {:.2}ms",
+            "Inserting block {} to schema {} postgres took {:.2}ms",
             self.slot,
             schema,
             started.elapsed().as_secs_f64() * 1000.0
