150346310563c5259e7470f4e6d93d0f0bc71318
GroovieGermanikus <groovie@mango.markets>
2023-12-07 16:17:25 +0100

    complete test

--- cluster-endpoints/examples/drain_to_tip_pattern.rs
+++ cluster-endpoints/examples/drain_to_tip_pattern.rs
@@ -34,7 +34,7 @@ async fn main() {
     send_stream(tx.clone()).await;
 
     // move tip; current tip is 3; next offered slot is 4
-    info!("Force tip to 6");
+    info!("==> force tip to 6 - expect progressor to unblock and offer 7");
     tx_tip.send(Message::new(6)).unwrap();
 
     info!("Blocking main thread for some time to allow the system to operate...");
@@ -48,7 +48,10 @@ async fn main() {
 }
 
 
-
+// this service is dedicated to one source channel which produces a monotonic stream of messages qualified by slot number
+// service maintains a tip variable which is updated by different part of system
+// service response to tip changes by blocking until the message received from stream has slot number greater than the tip
+// service "offers" this message to the rest of the system
 async fn start_progressor(mut blocks_notifier: Receiver<Message>, mut rx_tip: tokio::sync::watch::Receiver<Message>) {
     info!("Started progressor");
     tokio::spawn(async move {
@@ -66,14 +69,14 @@ async fn start_progressor(mut blocks_notifier: Receiver<Message>, mut rx_tip: to
                     }
                     local_tip = rx_tip.borrow_and_update().clone();
                     info!("++> tip changed to {}", local_tip);
-                    if local_tip.slot >= highest_block.slot {
-                        info!("!! next offered slot is invalid: {} >= {}", local_tip, highest_block.slot);
+                    if local_tip.slot <= highest_block.slot {
+                        info!("!! next offered slot is invalid: {} <= {}", highest_block.slot, local_tip);
                     }
                     // slow down in case of loop
                     // sleep(Duration::from_millis(100)).await;
                 }
                 recv_result = blocks_notifier.recv(), if !(highest_block.slot > local_tip.slot) => {
-                    debug!("block_after_tip.slot > local_tip.slot: {} > {}", highest_block.slot, local_tip.slot);
+                    debug!("!! block_after_tip.slot > local_tip.slot: {} > {}", highest_block.slot, local_tip.slot);
                     match recv_result {
                         Ok(msg) => {
                             info!("=> recv: {}", msg);
@@ -106,10 +109,9 @@ async fn send_stream(message_channel: Sender<Message>) {
 
     // drain 0 to 3; offer 4, then block
     for i in 0..10 {
-        info!("sending {}", i);
+        info!("> sending {} (queue size={})", i, message_channel.len());
         message_channel.send(Message::new(i)).unwrap();
-        info!("queue size: {}", message_channel.len());
-        sleep(Duration::from_millis(300)).await;
+        sleep(Duration::from_millis(100)).await;
     }
 
     assert_eq!(message_channel.len(), 5);
