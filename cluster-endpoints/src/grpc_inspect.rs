f16adc8a260a45101550d045717b2394b7417eab
GroovieGermanikus <groovie@mango.markets>
2024-01-22 14:53:34 +0100

    switch to blockhash instead of slot

--- cluster-endpoints/src/grpc_inspect.rs
+++ cluster-endpoints/src/grpc_inspect.rs
@@ -9,30 +9,34 @@ use tokio::sync::broadcast::error::RecvError;
 use tokio::task::JoinHandle;
 use tokio::time::sleep;
 
+
 // note: we assume that the invariants hold even right after startup
 pub fn block_debug_confirmation_levels(mut block_notifier: BlockStream) -> JoinHandle<()> {
     tokio::spawn(async move {
         let mut cleanup_before_slot = 0;
         let mut slots_since_last_cleanup = 0;
-        // TODO not sure if I should use blockhash instead of slot
-        let mut saw_processed_at: HashMap<Slot, SystemTime> = HashMap::new();
-        let mut saw_confirmed_at: HashMap<Slot, SystemTime> = HashMap::new();
-        let mut saw_finalized_at: HashMap<Slot, SystemTime> = HashMap::new();
+        // use blockhash as key instead of slot as for processed the slot is ambiguous
+        let mut saw_processed_at: HashMap<String, (Slot, SystemTime)> = HashMap::new();
+        let mut saw_confirmed_at: HashMap<String, (Slot, SystemTime)> = HashMap::new();
+        let mut saw_finalized_at: HashMap<String, (Slot, SystemTime)> = HashMap::new();
         'recv_loop: loop {
             match block_notifier.recv().await {
                 Ok(block) => {
-                    if block.slot < cleanup_before_slot {
+                    let slot = block.slot;
+                    let blockhash = block.blockhash.to_string();
+                    if slot < cleanup_before_slot {
                         continue 'recv_loop;
                     }
                     debug!(
-                        "Saw block: {} @ {} with {} txs",
-                        block.slot,
+                        "Saw block: {}#{} @ {} with {} txs",
+                        slot,
+                        blockhash,
                         block.commitment_config.commitment,
                         block.transactions.len()
                     );
 
                     if block.commitment_config.is_processed() {
-                        let prev_value = saw_processed_at.insert(block.slot, SystemTime::now());
+                        let prev_value = saw_processed_at.insert(blockhash.clone(), (slot, SystemTime::now()));
                         match prev_value {
                             None => {
                                 // okey
@@ -41,13 +45,12 @@ pub fn block_debug_confirmation_levels(mut block_notifier: BlockStream) -> JoinH
                                 // this is actually fatal
                                 error!(
                                     "should not see same processed slot twice ({}) - saw at {:?}",
-                                    block.slot, prev
-                                );
+                                    blockhash, format_timestamp(&prev.1));
                             }
                         }
                     }
                     if block.commitment_config.is_confirmed() {
-                        let prev_value = saw_confirmed_at.insert(block.slot, SystemTime::now());
+                        let prev_value = saw_confirmed_at.insert(blockhash.clone(), (slot, SystemTime::now()));
                         match prev_value {
                             None => {
                                 // okey
@@ -56,13 +59,13 @@ pub fn block_debug_confirmation_levels(mut block_notifier: BlockStream) -> JoinH
                                 // this is actually fatal
                                 error!(
                                     "should not see same confirmed slot twice ({}) - saw at {:?}",
-                                    block.slot, prev
+                                    blockhash, format_timestamp(&prev.1)
                                 );
                             }
                         }
                     }
                     if block.commitment_config.is_finalized() {
-                        let prev_value = saw_finalized_at.insert(block.slot, SystemTime::now());
+                        let prev_value = saw_finalized_at.insert(blockhash.clone(), (slot, SystemTime::now()));
                         match prev_value {
                             None => {
                                 // okey
@@ -71,7 +74,7 @@ pub fn block_debug_confirmation_levels(mut block_notifier: BlockStream) -> JoinH
                                 // this is actually fatal
                                 error!(
                                     "should not see same finalized slot twice ({}) - saw at {:?}",
-                                    block.slot, prev
+                                    slot, format_timestamp(&prev.1)
                                 );
                             }
                         }
@@ -79,15 +82,15 @@ pub fn block_debug_confirmation_levels(mut block_notifier: BlockStream) -> JoinH
 
                     // rule: if confirmed, we should have seen processed but not finalized
                     if block.commitment_config.is_confirmed() {
-                        if saw_processed_at.contains_key(&block.slot) {
+                        if saw_processed_at.contains_key(&blockhash) {
                             // okey
                         } else {
-                            error!("should not see confirmed slot without seeing processed slot first ({})", block.slot);
+                            error!("should not see confirmed slot without seeing processed slot first ({})", blockhash);
                         }
-                        if saw_finalized_at.contains_key(&block.slot) {
+                        if saw_finalized_at.contains_key(&blockhash) {
                             error!(
                                 "should not see confirmed slot after seeing finalized slot ({})",
-                                block.slot
+                                blockhash
                             );
                         } else {
                             // okey
@@ -96,18 +99,18 @@ pub fn block_debug_confirmation_levels(mut block_notifier: BlockStream) -> JoinH
 
                     // rule: if processed, we should have seen neither confirmed nor finalized
                     if block.commitment_config.is_processed() {
-                        if saw_confirmed_at.contains_key(&block.slot) {
+                        if saw_confirmed_at.contains_key(&blockhash) {
                             error!(
                                 "should not see processed slot after seeing confirmed slot ({})",
-                                block.slot
+                                blockhash
                             );
                         } else {
                             // okey
                         }
-                        if saw_finalized_at.contains_key(&block.slot) {
+                        if saw_finalized_at.contains_key(&blockhash) {
                             error!(
                                 "should not see processed slot after seeing finalized slot ({})",
-                                block.slot
+                                blockhash
                             );
                         } else {
                             // okey
@@ -116,27 +119,27 @@ pub fn block_debug_confirmation_levels(mut block_notifier: BlockStream) -> JoinH
 
                     // rule: if finalized, we should have seen processed and confirmed
                     if block.commitment_config.is_finalized() {
-                        if saw_processed_at.contains_key(&block.slot) {
+                        if saw_processed_at.contains_key(&blockhash) {
                             // okey
                         } else {
-                            error!("should not see finalized slot without seeing processed slot first ({})", block.slot);
+                            error!("should not see finalized slot without seeing processed slot first ({})", blockhash);
                         }
-                        if saw_confirmed_at.contains_key(&block.slot) {
+                        if saw_confirmed_at.contains_key(&blockhash) {
                             // okey
                         } else {
-                            error!("should not see finalized slot without seeing confirmed slot first ({})", block.slot);
+                            error!("should not see finalized slot without seeing confirmed slot first ({})", blockhash);
                         }
 
                         if let (Some(processed), Some(confirmed)) = (
-                            saw_processed_at.get(&block.slot),
-                            saw_confirmed_at.get(&block.slot),
+                            saw_processed_at.get(&blockhash),
+                            saw_confirmed_at.get(&blockhash),
                         ) {
-                            let finalized = saw_finalized_at.get(&block.slot).unwrap();
+                            let finalized = saw_finalized_at.get(&blockhash).unwrap();
                             debug!(
                                 "sequence: {:?} -> {:?} -> {:?}",
-                                format_timestamp(processed),
-                                format_timestamp(confirmed),
-                                format_timestamp(finalized)
+                                format_timestamp(&processed.1),
+                                format_timestamp(&confirmed.1),
+                                format_timestamp(&finalized.1)
                             );
                         }
                     }
@@ -145,10 +148,10 @@ pub fn block_debug_confirmation_levels(mut block_notifier: BlockStream) -> JoinH
                         slots_since_last_cleanup += 1;
                     } else {
                         // perform cleanup, THEN update cleanup_before_slot
-                        saw_processed_at.retain(|slot, _instant| *slot >= cleanup_before_slot);
-                        saw_confirmed_at.retain(|slot, _instant| *slot >= cleanup_before_slot);
-                        saw_finalized_at.retain(|slot, _instant| *slot >= cleanup_before_slot);
-                        cleanup_before_slot = block.slot - 200;
+                        saw_processed_at.retain(|_blockhash, (slot, _instant)| *slot >= cleanup_before_slot);
+                        saw_confirmed_at.retain(|_blockhash, (slot, _instant)| *slot >= cleanup_before_slot);
+                        saw_finalized_at.retain(|_blockhash, (slot, _instant)| *slot >= cleanup_before_slot);
+                        cleanup_before_slot = slot - 200;
                         debug!("move cleanup point to {}", cleanup_before_slot);
                         debug!(
                             "map sizes after cleanup: {} processed, {} confirmed, {} finalized",
