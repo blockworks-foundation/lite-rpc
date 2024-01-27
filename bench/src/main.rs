74a28c62b2fb91db377bf9bd0f20caa749294ebe
GroovieGermanikus <groovie@mango.markets>
2024-01-27 19:06:15 +0100

    fix debug/info

--- bench/src/main.rs
+++ bench/src/main.rs
@@ -17,11 +17,17 @@ use tokio::{
     sync::{mpsc::UnboundedSender, RwLock},
     time::{Duration, Instant},
 };
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 16)]
 async fn main() {
+    let env_filter = EnvFilter::builder()
+        .with_default_directive(LevelFilter::INFO.into())
+        .from_env().unwrap();
     tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::INFO).init();
+        .with_env_filter(env_filter)
+        .init();
 
     let Args {
         metrics_file_name,
