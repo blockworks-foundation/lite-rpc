55fb86f8e93dee9122a0d648a23779c4d577f59e
godmodegalactus <godmodegalactus@gmail.com>
2024-02-05 10:44:41 +0100

    Adding CLI argument to set the max number of connections with the leader

--- lite-rpc/src/cli.rs
+++ lite-rpc/src/cli.rs
@@ -67,6 +67,8 @@ pub struct Config {
     /// postgres config
     #[serde(default)]
     pub postgres: Option<PostgresSessionConfig>,
+
+    pub max_number_of_connection: Option<usize>,
 }
 
 impl Config {
@@ -172,6 +174,10 @@ impl Config {
         config.grpc_x_token4 = env::var("GRPC_X_TOKEN4")
             .map(Some)
             .unwrap_or(config.grpc_x_token4);
+
+        config.max_number_of_connection = env::var("MAX_NB_OF_CONNECTIONS")
+            .map(|x| x.parse().ok())
+            .unwrap_or(config.max_number_of_connection);
 
         config.postgres = PostgresSessionConfig::new_from_env()?.or(config.postgres);
 
