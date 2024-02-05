3e71242d095ede718643e1d83f59efb4d49b93c9
GroovieGermanikus <groovie@mango.markets>
2024-02-05 11:31:08 +0100

    clean deps

--- history/src/block_stores/postgres/mod.rs
+++ history/src/block_stores/postgres/mod.rs
@@ -2,7 +2,6 @@ pub mod postgres_block_store;
 pub use postgres_config::PostgresSessionConfig;
 pub use postgres_session::PostgresSession;
 pub use postgres_session::PostgresWriteSession;
-pub use postgres_epoch::PostgresEpoch;
 
 mod postgres_session;
 mod postgres_config;
