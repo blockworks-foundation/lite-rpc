6ffb7b070d71703418b8a2c66e4b41c09ca5510c
GroovieGermanikus <groovie@mango.markets>
2024-02-07 14:38:36 +0100

    roundtrip

--- cluster-endpoints/src/grpc_store_to_disk.rs
+++ cluster-endpoints/src/grpc_store_to_disk.rs
@@ -4,7 +4,9 @@ use solana_lite_rpc_core::types::BlockStream;
 use solana_sdk::clock::Slot;
 use solana_sdk::commitment_config::CommitmentConfig;
 use std::collections::HashMap;
-use std::{env, thread};
+use std::{env, io, process, thread};
+use std::fs::{create_dir, DirBuilder, File, OpenOptions};
+use std::io::{Read, Write};
 use std::path::{Path, PathBuf};
 use std::sync::atomic::AtomicU64;
 use std::sync::Arc;
@@ -12,16 +14,50 @@ use std::time::{Duration, SystemTime};
 use bytes::Bytes;
 use geyser_grpc_connector::{GeyserFilter, GrpcConnectionTimeouts, GrpcSourceConfig};
 use geyser_grpc_connector::grpc_subscription_autoreconnect_tasks::create_geyser_autoconnection_task;
+use tokio::io::BufWriter;
 use tokio::sync::broadcast::error::RecvError;
 use tokio::task::JoinHandle;
 use tokio::time::{sleep, Instant};
 use yellowstone_grpc_proto::geyser::subscribe_update::UpdateOneof;
+use yellowstone_grpc_proto::geyser::SubscribeUpdateBlock;
 use yellowstone_grpc_proto::prost::Message;
 use solana_lite_rpc_core::structures::produced_block::ProducedBlock;
+
+#[test]
+pub fn read_block_from_disk() {
+    tracing_subscriber::fmt::init();
+    // /Users/stefan/mango/code/lite-rpc/localdev-groovie-testing/blocks_on_disk/blocks-000251395xxx/block-000251395041-confirmed-1707312285514.dat
+
+    let file = Path::new("/Users/stefan/mango/code/lite-rpc/localdev-groovie-testing/blocks_on_disk/blocks-000251395xxx/block-000251395041-confirmed-1707312285514.dat");
+
+    let (slot_from_file, epoch_ms) = parse_slot_and_timestamp_from_file(file.file_name().unwrap().to_str().unwrap());
+    info!("slot from file: {}", slot_from_file);
+    info!("epochms from file: {}", epoch_ms);
+
+    let block_bytes = std::fs::read(file).unwrap();
+    info!("read {} bytes from block file", block_bytes.len());
+    let decoded = SubscribeUpdateBlock::decode(block_bytes.as_slice()).expect("Block file must be protobuf");
+
+
+    info!("decoded block: {}", decoded.slot);
+}
+
+// e.g. block-000251395041-confirmed-1707312285514.dat
+fn parse_slot_and_timestamp_from_file(file_name: &str) -> (Slot, u64) {
+
+    let slot_str = file_name.split("-").nth(1).unwrap();
+    let slot = slot_str.parse::<Slot>().unwrap();
+    let timestamp_str = file_name.split("-").nth(3).unwrap().split(".").next().unwrap();
+    info!("parsed slot {} from file name", slot);
+    let epoch_ms = timestamp_str.parse::<u64>().unwrap();
+    (slot, epoch_ms)
+}
+
 
 #[tokio::test]
 pub async fn read_stream() {
     tracing_subscriber::fmt::init();
+    configure_panic_hook();
 
     let grpc_addr = env::var("GRPC_ADDR").unwrap();
     let grpc_x_token = Some(env::var("GRPC_X_TOKEN").unwrap());
@@ -41,6 +77,7 @@ pub async fn read_stream() {
     );
 
     tokio::spawn(async move {
+        let blockstream_todisk = BlockStreamDumpOnDisk::create_with_root_path(Path::new("/Users/stefan/mango/code/lite-rpc/localdev-groovie-testing/blocks_on_disk"));
         loop {
             let now = Instant::now();
             match message_channel.recv().await {
@@ -52,6 +89,11 @@ pub async fn read_stream() {
                                 let mut buf = vec![];
                                 block_update.encode(&mut buf).expect("must be able to serialize to buffer");
                                 info!("got block update - {} bytes", buf.len());
+                                let meta = BlockStorageMeta {
+                                    slot: block_update.slot,
+                                    epoch_ms: now_epoch_ms(),
+                                };
+                                blockstream_todisk.write_block(&meta, &block_update);
                             }
                         }
                         geyser_grpc_connector::Message::Connecting(attempt) => {
@@ -63,8 +105,6 @@ pub async fn read_stream() {
                     error!("Stream closed - shutting down task");
                     return;
                 }
-
-
             }
         }
     });
@@ -109,12 +149,12 @@ pub fn spawn_block_todisk_writer(
 }
 
 
-pub struct ArchiveOnLocalDisk {
+pub struct BlockStreamDumpOnDisk {
     root_path: PathBuf,
 }
 
 // 1707307827380
-pub fn epoch_ms()  -> u64{
+pub fn now_epoch_ms() -> u64{
     SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_millis() as u64
 }
 
@@ -126,12 +166,12 @@ struct BlockStorageMeta {
 
 impl BlockStorageMeta {
     pub fn new_with_now(slot: Slot) -> Self {
-        let epoch_ms = epoch_ms();
+        let epoch_ms = now_epoch_ms();
         Self { slot, epoch_ms }
     }
 }
 
-impl ArchiveOnLocalDisk {
+impl BlockStreamDumpOnDisk {
     pub fn create_with_root_path(root_path: &Path) -> Self {
         let marker = Path::new(".solana-blocks-dump");
         assert!(
@@ -154,6 +194,29 @@ impl ArchiveOnLocalDisk {
         block_path
     }
 
+    fn write_block(&self, meta: &BlockStorageMeta, block_protobuf: &SubscribeUpdateBlock) {
+        let block_file = self.build_path(meta);
+        let block_dir = block_file.parent();
+
+        let create_result = create_dir(block_dir.unwrap());
+        if create_result.is_err() {
+            if create_result.err().unwrap().kind() != io::ErrorKind::AlreadyExists {
+                panic!("Must be able to create directory: {:?}", block_dir.unwrap());
+            }
+        }
+
+        let block_bytes = block_protobuf.encode_to_vec();
+        let mut new_block_file =
+            OpenOptions::new()
+                .write(true)
+                .create_new(true)
+                .open(block_file.clone())
+                .expect("Must be able to create new file");
+        new_block_file.write(block_bytes.as_slice())
+            .expect("must be able to write block to disk");
+        info!("Wrote block {} with {} bytes to disk: {}", meta.slot, block_bytes.len(), block_file.display());
+    }
+
     // 246300234 -> 246300xxx
     pub fn format_slot_1k(slot: Slot) -> String {
         format!("{}xxx", slot / 1000)
@@ -163,21 +226,45 @@ impl ArchiveOnLocalDisk {
 
 
 
+
 #[test]
-fn runit() {
+fn build_path() {
     tracing_subscriber::fmt::init();
 
     let root_path = Path::new("/Users/stefan/mango/code/lite-rpc/localdev-groovie-testing/blocks_on_disk");
     let blocks_on_disk =
-        ArchiveOnLocalDisk::create_with_root_path(root_path);
+        BlockStreamDumpOnDisk::create_with_root_path(root_path);
 
     let block_storage_meta = BlockStorageMeta::new_with_now(246300234);
 
-    info!("blocks_on_disk {:?}", blocks_on_disk.build_path(&block_storage_meta));
+    let out_file = blocks_on_disk.build_path(&block_storage_meta);
+    info!("blocks_on_disk {:?}", out_file);
+
+    info!("dir: {:?}", out_file.parent().unwrap());
 }
 
 #[test]
 fn format_slot_xxx() {
-    assert_eq!("246300xxx", ArchiveOnLocalDisk::format_slot_1k(246300234));
+    assert_eq!("246300xxx", BlockStreamDumpOnDisk::format_slot_1k(246300234));
 }
 
+#[test]
+fn parse_slot_and_epochms() {
+    let file_name = "block-000251395041-confirmed-1707312285514.dat";
+    let (slot_from_file, epoch_ms) = parse_slot_and_timestamp_from_file(file_name);
+    info!("slot from file: {}", slot_from_file);
+    info!("epochms from file: {}", epoch_ms);
+
+}
+
+
+fn configure_panic_hook() {
+    let default_panic = std::panic::take_hook();
+    std::panic::set_hook(Box::new(move |panic_info| {
+        default_panic(panic_info);
+        // e.g. panicked at 'BANG', lite-rpc/tests/blockstore_integration_tests:260:25
+        error!("{}", panic_info);
+        eprintln!("{}", panic_info);
+        process::exit(12);
+    }));
+}
