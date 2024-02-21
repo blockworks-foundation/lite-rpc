65fef0fae3dc457980b43539e029b1ee02f1cd18
GroovieGermanikus <groovie@mango.markets>
2024-02-21 11:48:37 +0100

    log jemalloc allocations

--- lite-rpc/src/main.rs
+++ lite-rpc/src/main.rs
@@ -416,6 +416,25 @@ pub async fn main() -> anyhow::Result<()> {
 
     let config = Config::load().await?;
 
+
+    std::thread::spawn(|| {
+        let e = tikv_jemalloc_ctl::epoch::mib().unwrap();
+        let allocated = tikv_jemalloc_ctl::stats::allocated::mib().unwrap();
+        let resident = tikv_jemalloc_ctl::stats::resident::mib().unwrap();
+        loop {
+            // Many statistics are cached and only updated
+            // when the epoch is advanced:
+            e.advance().unwrap();
+
+            // Read statistics using MIB key:
+            let allocated = allocated.read().unwrap();
+            let resident = resident.read().unwrap();
+            debug!("MEMORY ALLOCATOR STATS: {:.1}MB allocated / {:.1}MB resident",
+                (allocated / 1024) as f64 / 1024.0, (resident / 1024) as f64 / 1024.0);
+            std::thread::sleep(std::time::Duration::from_secs(2));
+        }
+    });
+
     let ctrl_c_signal = tokio::signal::ctrl_c();
     let Config { rpc_addr, .. } = &config;
     // rpc client
