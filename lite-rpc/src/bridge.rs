ddcad1e55bf52770e37ddf000ce9548249fa5ed5
godmodegalactus <godmodegalactus@gmail.com>
2024-02-27 19:31:36 +0100

    Adding get balance call

--- lite-rpc/src/bridge.rs
+++ lite-rpc/src/bridge.rs
@@ -613,4 +613,42 @@ impl LiteRpcServer for LiteBridge {
             Err(jsonrpsee::types::error::ErrorCode::MethodNotFound.into())
         }
     }
+
+    async fn get_balance(
+        &self,
+        pubkey_str: String,
+        config: Option<RpcContextConfig>,
+    ) -> RpcResult<RpcResponse<u64>> {
+        let Ok(pubkey) = Pubkey::from_str(&pubkey_str) else {
+            // pubkey is invalid
+            return Err(jsonrpsee::types::error::ErrorCode::InvalidParams.into());
+        };
+        let config = config.map(|x| RpcAccountInfoConfig {
+            encoding: None,
+            data_slice: None,
+            commitment: x.commitment,
+            min_context_slot: x.min_context_slot,
+        });
+        if let Some(account_service) = &self.accounts_service {
+            match account_service.get_account(pubkey, config).await {
+                Ok((slot, ui_account)) => Ok(RpcResponse {
+                    context: RpcResponseContext {
+                        slot,
+                        api_version: None,
+                    },
+                    value: ui_account.map(|x| x.lamports).unwrap_or_default(),
+                }),
+                Err(_) => {
+                    // account not found
+                    Err(jsonrpsee::types::error::ErrorCode::ServerError(
+                        RpcErrors::AccountNotFound as i32,
+                    )
+                    .into())
+                }
+            }
+        } else {
+            // accounts are disabled
+            Err(jsonrpsee::types::error::ErrorCode::MethodNotFound.into())
+        }
+    }
 }
