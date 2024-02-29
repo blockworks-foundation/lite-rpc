31dc32fc4bf0c1aee57fbdc21e17477dfe610f91
GroovieGermanikus <groovie@mango.markets>
2024-02-29 09:11:06 +0100

    restore original version

--- cluster-endpoints/src/grpc_subscription.rs
+++ cluster-endpoints/src/grpc_subscription.rs
@@ -67,7 +67,7 @@ impl LoggingTimer {
 
 
 /// grpc version of ProducedBlock mapping
-pub fn from_grpc_block_update_old(
+pub fn from_grpc_block_update_reimplement(
     block: SubscribeUpdateBlock,
     commitment_config: CommitmentConfig,
 ) -> ProducedBlock {
@@ -80,7 +80,7 @@ pub fn from_grpc_block_update_old(
         .transactions
         .into_iter()
         // .into_par_iter() TODO
-        .filter_map(|tx| maptx_old(tx))
+        .filter_map(|tx| maptx_reimplemented(tx))
         .collect();
     log_timer.log_if_exceed("after transactions");
     println!("tx count {}", txs.len());
@@ -263,6 +263,238 @@ pub fn create_grpc_subscription(
 }
 
 
+pub fn from_grpc_block_update_original(
+    block: SubscribeUpdateBlock,
+    commitment_config: CommitmentConfig,
+) -> ProducedBlock {
+    let _span = debug_span!("from_grpc_block_update", ?block.slot).entered();
+    let txs: Vec<TransactionInfo> = block
+        .transactions
+        .into_iter()
+        .filter_map(|tx| {
+            let meta = tx.meta?;
+
+            let transaction = tx.transaction?;
+
+            let message = transaction.message?;
+
+            let header = message.header?;
+
+            let signatures = transaction
+                .signatures
+                .into_iter()
+                .filter_map(|sig| match Signature::try_from(sig) {
+                    Ok(sig) => Some(sig),
+                    Err(_) => {
+                        log::warn!(
+                            "Failed to read signature from transaction in block {} - skipping",
+                            block.blockhash
+                        );
+                        None
+                    }
+                })
+                .collect_vec();
+
+            let err = meta.err.map(|x| {
+                bincode::deserialize::<TransactionError>(&x.err)
+                    .expect("TransactionError should be deserialized")
+            });
+
+            let signature = signatures[0];
+            let compute_units_consumed = meta.compute_units_consumed;
+            let account_keys: Vec<Pubkey> = message
+                .account_keys
+                .into_iter()
+                .map(|key| {
+                    let bytes: [u8; 32] = key.try_into().unwrap_or(Pubkey::default().to_bytes());
+                    Pubkey::new_from_array(bytes)
+                })
+                .collect();
+
+            let message = VersionedMessage::V0(v0::Message {
+                header: MessageHeader {
+                    num_required_signatures: header.num_required_signatures as u8,
+                    num_readonly_signed_accounts: header.num_readonly_signed_accounts as u8,
+                    num_readonly_unsigned_accounts: header.num_readonly_unsigned_accounts as u8,
+                },
+                account_keys: account_keys.clone(),
+                recent_blockhash: Hash::new(&message.recent_blockhash),
+                instructions: message
+                    .instructions
+                    .into_iter()
+                    .map(|ix| CompiledInstruction {
+                        program_id_index: ix.program_id_index as u8,
+                        accounts: ix.accounts,
+                        data: ix.data,
+                    })
+                    .collect(),
+                address_table_lookups: message
+                    .address_table_lookups
+                    .into_iter()
+                    .map(|table| {
+                        let bytes: [u8; 32] = table
+                            .account_key
+                            .try_into()
+                            .unwrap_or(Pubkey::default().to_bytes());
+                        MessageAddressTableLookup {
+                            account_key: Pubkey::new_from_array(bytes),
+                            writable_indexes: table.writable_indexes,
+                            readonly_indexes: table.readonly_indexes,
+                        }
+                    })
+                    .collect(),
+            });
+
+            let legacy_compute_budget: Option<(u32, Option<u64>)> =
+                message.instructions().iter().find_map(|i| {
+                    if i.program_id(message.static_account_keys())
+                        .eq(&compute_budget::id())
+                    {
+                        if let Ok(ComputeBudgetInstruction::RequestUnitsDeprecated {
+                                      units,
+                                      additional_fee,
+                                  }) = try_from_slice_unchecked(i.data.as_slice())
+                        {
+                            if additional_fee > 0 {
+                                return Some((
+                                    units,
+                                    Some(((units * 1000) / additional_fee) as u64),
+                                ));
+                            } else {
+                                return Some((units, None));
+                            }
+                        }
+                    }
+                    None
+                });
+
+            let legacy_cu_requested = legacy_compute_budget.map(|x| x.0);
+            let legacy_prioritization_fees = legacy_compute_budget.map(|x| x.1).unwrap_or(None);
+
+            let cu_requested = message
+                .instructions()
+                .iter()
+                .find_map(|i| {
+                    if i.program_id(message.static_account_keys())
+                        .eq(&compute_budget::id())
+                    {
+                        if let Ok(ComputeBudgetInstruction::SetComputeUnitLimit(limit)) =
+                            try_from_slice_unchecked(i.data.as_slice())
+                        {
+                            return Some(limit);
+                        }
+                    }
+                    None
+                })
+                .or(legacy_cu_requested);
+
+            let prioritization_fees = message
+                .instructions()
+                .iter()
+                .find_map(|i| {
+                    if i.program_id(message.static_account_keys())
+                        .eq(&compute_budget::id())
+                    {
+                        if let Ok(ComputeBudgetInstruction::SetComputeUnitPrice(price)) =
+                            try_from_slice_unchecked(i.data.as_slice())
+                        {
+                            return Some(price);
+                        }
+                    }
+
+                    None
+                })
+                .or(legacy_prioritization_fees);
+
+            let is_vote_transaction = message.instructions().iter().any(|i| {
+                i.program_id(message.static_account_keys())
+                    .eq(&solana_sdk::vote::program::id())
+                    && limited_deserialize::<VoteInstruction>(&i.data)
+                    .map(|vi| vi.is_simple_vote())
+                    .unwrap_or(false)
+            });
+
+            let readable_accounts = account_keys
+                .iter()
+                .enumerate()
+                .filter(|(index, _)| !message.is_maybe_writable(*index))
+                .map(|(_, pk)| *pk)
+                .collect();
+            let writable_accounts = account_keys
+                .iter()
+                .enumerate()
+                .filter(|(index, _)| message.is_maybe_writable(*index))
+                .map(|(_, pk)| *pk)
+                .collect();
+
+            let address_lookup_tables = message
+                .address_table_lookups()
+                .map(|x| x.to_vec())
+                .unwrap_or_default();
+
+            Some(TransactionInfo {
+                signature: signature.to_string(),
+                is_vote: is_vote_transaction,
+                err,
+                cu_requested,
+                prioritization_fees,
+                cu_consumed: compute_units_consumed,
+                recent_blockhash: message.recent_blockhash().to_string(),
+                message: BASE64.encode(message.serialize()),
+                readable_accounts,
+                writable_accounts,
+                address_lookup_tables,
+            })
+        })
+        .collect();
+
+    let rewards = block.rewards.map(|rewards| {
+        rewards
+            .rewards
+            .into_iter()
+            .map(|reward| Reward {
+                pubkey: reward.pubkey.to_owned(),
+                lamports: reward.lamports,
+                post_balance: reward.post_balance,
+                reward_type: match reward.reward_type() {
+                    yellowstone_grpc_proto::prelude::RewardType::Unspecified => None,
+                    yellowstone_grpc_proto::prelude::RewardType::Fee => Some(RewardType::Fee),
+                    yellowstone_grpc_proto::prelude::RewardType::Rent => Some(RewardType::Rent),
+                    yellowstone_grpc_proto::prelude::RewardType::Staking => {
+                        Some(RewardType::Staking)
+                    }
+                    yellowstone_grpc_proto::prelude::RewardType::Voting => Some(RewardType::Voting),
+                },
+                commission: None,
+            })
+            .collect_vec()
+    });
+
+    let leader_id = if let Some(rewards) = &rewards {
+        rewards
+            .iter()
+            .find(|reward| Some(RewardType::Fee) == reward.reward_type)
+            .map(|leader_reward| leader_reward.pubkey.clone())
+    } else {
+        None
+    };
+
+    let inner = ProducedBlockInner {
+        transactions: txs,
+        block_height: block
+            .block_height
+            .map(|block_height| block_height.block_height)
+            .unwrap(),
+        block_time: block.block_time.map(|time| time.timestamp).unwrap() as u64,
+        blockhash: block.blockhash,
+        previous_blockhash: block.parent_blockhash,
+        leader_id,
+        parent_slot: block.parent_slot,
+        slot: block.slot,
+        rewards,
+    };
+    ProducedBlock::new(inner, commitment_config)
+}
 
 #[cfg(test)]
 mod tests {
@@ -285,7 +517,7 @@ mod tests {
         // info!("example_block: {:?}", example_block);
 
         let started_at = Instant::now();
-        let _produced_block = from_grpc_block_update_old(example_block, CommitmentConfig::confirmed());
+        let _produced_block = from_grpc_block_update_reimplement(example_block, CommitmentConfig::confirmed());
         println!("from_grpc_block_update mapping took: {:?}", started_at.elapsed());
     }
 
@@ -301,8 +533,8 @@ mod tests {
 
         let started_at = Instant::now();
         println!("from_grpc_block_update mapping took: {:?}", started_at.elapsed());
-        let produced_block_new = from_grpc_block_update_optimized(example_block1, CommitmentConfig::confirmed());
-        let produced_block_old = from_grpc_block_update_old(example_block2, CommitmentConfig::confirmed());
+        let produced_block_old = from_grpc_block_update_original(example_block1, CommitmentConfig::confirmed());
+        let produced_block_new = from_grpc_block_update_reimplement(example_block2, CommitmentConfig::confirmed());
 
 
         if format!("{:?}",produced_block_old) != format!("{:?}", produced_block_new) {
@@ -322,7 +554,7 @@ mod tests {
 
         let raw_block = include_bytes!("block-000251402816-confirmed-1707315774189.dat");
         let example_block = SubscribeUpdateBlock::decode(raw_block.as_slice()).expect("Block file must be protobuf");
-        let produced_block = from_grpc_block_update_old(example_block, CommitmentConfig::confirmed());
+        let produced_block = from_grpc_block_update_reimplement(example_block, CommitmentConfig::confirmed());
         let raw = &produced_block.transactions[0].message;
         // BASE64.encode(message.serialize())
         let vec: Vec<u8> = BASE64.decode(raw).unwrap();
@@ -339,7 +571,7 @@ mod tests {
     }
 }
 
-fn maptx_old(tx: SubscribeUpdateTransactionInfo) -> Option<TransactionInfo> {
+fn maptx_reimplemented(tx: SubscribeUpdateTransactionInfo) -> Option<TransactionInfo> {
 
     let log_timer_tx = LoggingTimer { started_at: Instant::now(), threshold: Duration::from_millis(10) };
     log_timer_tx.log_if_exceed("start");
@@ -351,21 +583,6 @@ fn maptx_old(tx: SubscribeUpdateTransactionInfo) -> Option<TransactionInfo> {
 
     let header = message.header?;
 
-    // TODO remove
-    let signatures_old = transaction
-        .signatures
-        .into_iter()
-        .filter_map(|sig| match Signature::try_from(sig) {
-            Ok(sig) => Some(sig),
-            Err(_) => {
-                log::warn!(
-                            "Failed to read signature from transaction in block TBD - skipping",
-                            // block.blockhash // TODO
-                        );
-                None
-            }
-        })
-        .collect_vec();
     log_timer_tx.log_if_exceed("after signatures");
 
     let err = meta.err.map(|x| {
@@ -374,14 +591,13 @@ fn maptx_old(tx: SubscribeUpdateTransactionInfo) -> Option<TransactionInfo> {
     });
     log_timer_tx.log_if_exceed("after err");
 
+    // let signature = signatures_old[0];
     let signature = {
         let sig_bytes: [u8; 64] = tx.signature.try_into().unwrap();
         let signature = Signature::from(sig_bytes);
-        assert_eq!(signature, signatures_old[0]);
         signature
     };
 
-    // let signature = signatures_old[0];
     let compute_units_consumed = meta.compute_units_consumed;
     let account_keys: Vec<Pubkey> = message
         .account_keys
