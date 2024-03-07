bfe1abed39f62992fa58144b96651f994d191bd9
GroovieGermanikus <groovie@mango.markets>
2024-03-07 19:45:57 +0100

    wip tests

--- lite-rpc/src/bridge_json_test.rs
+++ lite-rpc/src/bridge_json_test.rs
@@ -19,12 +19,21 @@ use crate::errors::JsonRpcError;
 
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr;
     use assert_json_diff::{assert_json_eq, assert_json_include};
     use bincode::options;
     use serde_json::Value;
+    use solana_sdk::blake3::Hash;
     use solana_sdk::clock::{Slot, UnixTimestamp};
+    use solana_sdk::hash::Hasher;
+    use solana_sdk::instruction::CompiledInstruction;
+    use solana_sdk::message::{legacy, LegacyMessage};
+    use solana_sdk::reward_type::RewardType;
+    use solana_sdk::system_instruction::SystemInstruction;
     use solana_sdk::transaction::{TransactionVersion, VersionedTransaction};
-    use solana_transaction_status::{BlockEncodingOptions, EncodedTransaction, TransactionBinaryEncoding, UiConfirmedBlock};
+    use solana_sdk::vote::instruction::VoteInstruction;
+    use solana_transaction_status::{BlockEncodingOptions, EncodedTransaction, Reward, TransactionBinaryEncoding, UiConfirmedBlock};
+    use solana_transaction_status::TransactionDetails::Signatures;
     use super::*;
 
     #[test]
@@ -146,14 +155,83 @@ mod tests {
 
 
     #[test]
+    fn test_decode() {
+
+
+        let foo = "Fk63Q321enfa7mN4TibcZkzYHBEhvTgZ4W1c4r\
+            zdbaHKAQ3262j4bLyCTdHwFx3dSC76VmdbGa4PpAY2p\
+            XgJZuPRnvttdJJLTQoaymujmbGqMe5gMXNcJ15AzXJN\
+            DcZNBWFpvFPTFENGrRznFy6WmosM2xMB7D";
+
+        let raw = bs58::decode(&foo).into_vec().unwrap();
+
+        let obj = bincode::deserialize::<VoteInstruction>(&raw).unwrap();
+        obj.
+
+    }
+
+    #[test]
     fn test_get_block_with_versioned_tx() {
+        // block 256912126 from testnet, 2024-03-07
+
+        let signature1 = Signature::from_str("3mJ2rMus6q9XEtgrbnnAfcXYtZrsjo4ehdHPkVWbM78YACUwvr9XkXLnYTtaqZ98S1gft6PrcCqMis3WuBtacPv3").unwrap();
+
+
+        let ins1 = CompiledInstruction::new(
+            2, &0, vec![1,0]);
+
+        let message1 = VersionedMessage::Legacy(legacy::Message {
+            header: MessageHeader {
+                num_required_signatures: 1,
+                num_readonly_signed_accounts: 0,
+                num_readonly_unsigned_accounts: 1,
+            },
+            account_keys: vec![
+                Pubkey::from_str("AweDwMst78mJA1pWyqmueYEns6Mdm9QDNv4SpZJdDaNs").unwrap(),
+                Pubkey::from_str("J5RP9MhwfrFzA9Qfm8DUAzeFgVMEjjah77zGFNZC9HJv").unwrap(),
+                Pubkey::from_str("Vote111111111111111111111111111111111111111").unwrap(),
+            ],
+            recent_blockhash: solana_sdk::hash::Hash::from_str("EgPc6CgZPTtqy1wUJTbbU3TZF2SsCFWHCRrzvnufWjtb").unwrap(),
+            instructions: vec![ins1],
+        });
+
+        let tx1: TransactionWithStatusMeta = TransactionWithStatusMeta::Complete(
+            VersionedTransactionWithStatusMeta {
+                transaction: VersionedTransaction {
+                    signatures: vec![signature1], // TODO check if that is correct
+                    message: message1,
+                },
+                meta: TransactionStatusMeta {
+                    status: Ok(()),
+                    fee: 5000,
+                    pre_balances: vec![5219591194, 145052450, 1],
+                    post_balances: vec![5219586194, 145052450, 1],
+                    inner_instructions: Some(vec![]),
+                    log_messages: Some(vec![
+                        "Program Vote111111111111111111111111111111111111111 invoke [1]".to_string(),
+                        "Program Vote111111111111111111111111111111111111111 success".to_string()]),
+                    pre_token_balances: Some(vec![]),
+                    post_token_balances: Some(vec![]),
+                    rewards: Some(vec![]),
+                    loaded_addresses: Default::default(),
+                    return_data: None,
+                    compute_units_consumed: Some(2100),
+                },
+            }
+        );
 
         let confirmed_block = ConfirmedBlock {
             previous_blockhash: "Egoc9s7MWfAUrxt2kCat7oGik49mwdKbBDsZ1KgBPTcb".to_string(),
             blockhash: "APBi3GZqo24ya8Fm4nWfRU2vMZSaaGLbXissLi2GA3Fh".to_string(),
             parent_slot: 256912125,
-            transactions: vec![], // TODO fill
-            rewards: vec![],
+            transactions: vec![tx1], // TODO fill
+            rewards: vec![Reward {
+                pubkey: "8HL5VpqGfTG9SVkHyWU9gjo4xdQYbbdVS7ExTrou3zCE".to_string(),
+                lamports: 12042400,
+                post_balance: 31559596374,
+                reward_type: Some(RewardType::Fee),
+                commission: None
+            }],
             block_time: Some(1709807265 as UnixTimestamp),
             block_height: Some(220980772),
         };
@@ -163,15 +241,93 @@ mod tests {
             show_rewards: true,
             max_supported_transaction_version: Some(0),
         };
-        let confirmed_block = confirmed_block.encode_with_options(UiTransactionEncoding::Base58, options).unwrap();
+        let confirmed_block = confirmed_block.encode_with_options(UiTransactionEncoding::Json, options).unwrap();
 
         assert_json_eq!(
             confirmed_block,
             serde_json::from_str::<Value>(
-            r#"{"previousBlockhash":"Egoc9s7MWfAUrxt2kCat7oGik49mwdKbBDsZ1KgBPTcb",
-                "blockhash":"APBi3GZqo24ya8Fm4nWfRU2vMZSaaGLbXissLi2GA3Fh",
-                "parentSlot":256912125,"transactions":[],"rewards":[],"blockTime":1709807265,"blockHeight":220980772
-            }"#).unwrap());
+            r#"
+            {
+              "previousBlockhash": "Egoc9s7MWfAUrxt2kCat7oGik49mwdKbBDsZ1KgBPTcb",
+              "blockhash": "APBi3GZqo24ya8Fm4nWfRU2vMZSaaGLbXissLi2GA3Fh",
+              "parentSlot": 256912125,
+              "transactions": [
+                {
+                  "transaction": {
+                    "signatures": [
+                      "3mJ2rMus6q9XEtgrbnnAfcXYtZrsjo4ehdHPkVWbM78YACUwvr9XkXLnYTtaqZ98S1gft6PrcCqMis3WuBtacPv3"
+                    ],
+                    "message": {
+                      "header": {
+                        "numRequiredSignatures": 1,
+                        "numReadonlySignedAccounts": 0,
+                        "numReadonlyUnsignedAccounts": 1
+                      },
+                      "accountKeys": [
+                        "AweDwMst78mJA1pWyqmueYEns6Mdm9QDNv4SpZJdDaNs",
+                        "J5RP9MhwfrFzA9Qfm8DUAzeFgVMEjjah77zGFNZC9HJv",
+                        "Vote111111111111111111111111111111111111111"
+                      ],
+                      "recentBlockhash": "EgPc6CgZPTtqy1wUJTbbU3TZF2SsCFWHCRrzvnufWjtb",
+                      "instructions": [
+                        {
+                          "programIdIndex": 2,
+                          "accounts": [
+                            1,
+                            0
+                          ],
+                          "data": "Fk63Q321enfa7mN4TibcZkzYHBEhvTgZ4W1c4rzdbaHKAQ3262j4bLyCTdHwFx3dSC76VmdbGa4PpAY2pXgJZuPRnvttdJJLTQoaymujmbGqMe5gMXNcJ15AzXJNDcZNBWFpvFPTFENGrRznFy6WmosM2xMB7D",
+                          "stackHeight": null
+                        }
+                      ]
+                    }
+                  },
+                  "meta": {
+                    "err": null,
+                    "status": {
+                      "Ok": null
+                    },
+                    "fee": 5000,
+                    "preBalances": [
+                      5219591194,
+                      145052450,
+                      1
+                    ],
+                    "postBalances": [
+                      5219586194,
+                      145052450,
+                      1
+                    ],
+                    "innerInstructions": [],
+                    "logMessages": [
+                      "Program Vote111111111111111111111111111111111111111 invoke [1]",
+                      "Program Vote111111111111111111111111111111111111111 success"
+                    ],
+                    "preTokenBalances": [],
+                    "postTokenBalances": [],
+                    "rewards": [],
+                    "loadedAddresses": {
+                      "writable": [],
+                      "readonly": []
+                    },
+                    "computeUnitsConsumed": 2100
+                  },
+                  "version": "legacy"
+                }
+              ],
+              "rewards": [
+                {
+                  "pubkey": "8HL5VpqGfTG9SVkHyWU9gjo4xdQYbbdVS7ExTrou3zCE",
+                  "lamports": 12042400,
+                  "postBalance": 31559596374,
+                  "rewardType": "Fee",
+                  "commission": null
+                }
+              ],
+              "blockTime": 1709807265,
+              "blockHeight": 220980772
+            }
+            "#).unwrap());
     }
 
     fn assert_same_response_success(slot: u64, config: RpcBlockConfig) {
