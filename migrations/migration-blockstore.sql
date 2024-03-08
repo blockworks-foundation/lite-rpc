693f344d2213ed799d8c04f71ec2fb390e20ed5f
GroovieGermanikus <groovie@mango.markets>
2024-03-08 10:03:33 +0100

    brush up migrations

--- migrations/migration-blockstore.sql
+++ migrations/migration-blockstore.sql
@@ -1,9 +1,8 @@
 -- backported patches; put latest on bottom
 
 -- add index of transaction in block
-ALTER TABLE rpc2a_epoch_607.transaction_blockdata ADD COLUMN idx int4;
-UPDATE rpc2a_epoch_607.transaction_blockdata set idx=0 WHERE idx is null;
-ALTER TABLE rpc2a_epoch_607.transaction_blockdata SET NOT NULL;
+ALTER TABLE rpc2a_epoch_607.transaction_blockdata ADD COLUMN idx int4 DEFAULTL 0;
+ALTER TABLE rpc2a_epoch_607.transaction_blockdata ALTER COLUMN idx DROP DEFAULT;
 
 ALTER TABLE rpc2a_epoch_607.transaction_blockdata ADD COLUMN message_version int4 DEFAULT -2020;
 ALTER TABLE rpc2a_epoch_607.transaction_blockdata ALTER COLUMN message_version DROP DEFAULT;
