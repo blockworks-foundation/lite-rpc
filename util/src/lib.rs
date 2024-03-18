aaa80e5d23da942e034789b503a04498c7118715
GroovieGermanikus <groovie@mango.markets>
2024-03-18 19:48:40 +0100

    add obfuscation util

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -1,3 +1,2 @@
 pub mod encoding;
 mod secrets;
-
--- util/src/secrets.rs
+++ util/src/secrets.rs
@@ -1,12 +1,9 @@
-
 pub fn obfuscate_rpcurl(rpc_addr: &str) -> String {
     if rpc_addr.contains("rpcpool.com") {
         return rpc_addr.replacen(char::is_numeric, "X", 99);
     }
     rpc_addr.to_string()
 }
-
-
 
 pub fn obfuscate_token(token: &Option<String>) -> String {
     match token {
