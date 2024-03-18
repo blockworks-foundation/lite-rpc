f51282ab5b104643bddc0264e85fbc9435d70c12
GroovieGermanikus <groovie@mango.markets>
2024-03-18 13:39:24 +0100

    WIP

--- benchrunner-service/src/main.rs
+++ benchrunner-service/src/main.rs
@@ -1,12 +1,16 @@
 mod prometheus_sync;
+mod postgres_session;
 
 use std::net::SocketAddr;
 use std::ops::AddAssign;
 use std::str::FromStr;
+use std::sync::Arc;
 use std::time::Duration;
 use log::info;
 use prometheus::{Gauge, IntGauge, opts, register_gauge, register_int_gauge};
+use tokio::sync::mpsc::Sender;
 use tokio_cron_scheduler::{Job, JobScheduler, JobSchedulerError};
+use crate::postgres_session::{PostgresSession, PostgresSessionConfig};
 use crate::prometheus_sync::PrometheusSync;
 
 
@@ -24,13 +28,20 @@ lazy_static::lazy_static! {
 async fn main() -> Result<(), JobSchedulerError> {
     tracing_subscriber::fmt::init();
 
-    let prometheus_task = PrometheusSync::sync(SocketAddr::from_str("[::]:9091").unwrap());
+    // TODO make it optional
+    let postgres_config = PostgresSessionConfig::new_from_env().unwrap();
+    let postgres_session = Arc::new(PostgresSession::new(postgres_config.unwrap()).await.unwrap());
+
+    let _prometheus_task = PrometheusSync::sync(SocketAddr::from_str("[::]:9091").unwrap());
 
     let mut sched = JobScheduler::new().await?;
 
-    // Add basic cron job
+    // TODO check if runs are interleaved
     sched.add(
-        Job::new_async("1/2 * * * * *", |_uuid, _l| Box::pin(work()))?
+        Job::new_async("1/2 * * * * *", move |_uuid, _l| {
+
+
+            Box::pin(work(postgres_session.clone()) )})?
     ).await?;
 
 
@@ -43,11 +54,15 @@ async fn main() -> Result<(), JobSchedulerError> {
     Ok(())
 }
 
-async fn work() {
-
-
+async fn work(postgres_session: Arc<PostgresSession>) {
     info!("I run every 2 seconds");
 
     PROM_TX_SENT.set(80);
+    PROM_TXS_CONFIRMED.set(80);
+    PROM_TXS_UN_CONFIRMED.set(20);
+    PROM_AVG_CONFIRM.set(2500.0);
+
+    let row = postgres_session.client.query_one("SELECT 1", &[]).await.unwrap();
+    info!("row: {:?}", row.get::<_, i32>(0));
 
 }
---
+++ migrations/create_benchrunner.sql
@@ -0,0 +1,3 @@
+
+CREATE SCHEMA benchrunner;
+
