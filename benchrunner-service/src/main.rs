f959e182289dfdbd039bcecc051e66de71759415
GroovieGermanikus <groovie@mango.markets>
2024-03-18 11:27:33 +0100

    wip

--- benchrunner-service/src/main.rs
+++ benchrunner-service/src/main.rs
@@ -1,17 +1,36 @@
+mod prometheus_sync;
+
+use std::net::SocketAddr;
+use std::ops::AddAssign;
+use std::str::FromStr;
 use std::time::Duration;
+use log::info;
+use prometheus::{Gauge, IntGauge, opts, register_gauge, register_int_gauge};
 use tokio_cron_scheduler::{Job, JobScheduler, JobSchedulerError};
+use crate::prometheus_sync::PrometheusSync;
+
+
+// https://github.com/blockworks-foundation/lite-rpc/blob/production/bench/src/metrics.rs
+lazy_static::lazy_static! {
+    static ref PROM_TX_SENT: IntGauge = register_int_gauge!(opts!("literpc_benchrunner_txs_sent", "Total number of transactions sent")).unwrap();
+    static ref PROM_TXS_CONFIRMED: IntGauge = register_int_gauge!(opts!("literpc_benchrunner_txs_confirmed", "Number of transactions confirmed")).unwrap();
+    static ref PROM_TXS_UN_CONFIRMED: IntGauge = register_int_gauge!(opts!("literpc_benchrunner_txs_un_confirmed", "Number of transactions not confirmed")).unwrap();
+    static ref PROM_AVG_CONFIRM: Gauge = register_gauge!(opts!("literpc_benchrunner_avg_confirmation_time", "Confirmation time(ms)")).unwrap();
+    // static ref RPC_RESPONDING: Gauge = register_gauge!(opts!("literpc_benchrunner_send_tps", "Transactions")).unwrap();
+    // TODO add more
+}
 
 #[tokio::main]
 async fn main() -> Result<(), JobSchedulerError> {
     tracing_subscriber::fmt::init();
 
+    let prometheus_task = PrometheusSync::sync(SocketAddr::from_str("[::]:9091").unwrap());
+
     let mut sched = JobScheduler::new().await?;
 
     // Add basic cron job
     sched.add(
-        Job::new("1/2 * * * * *", |_uuid, _l| {
-            println!("I run every 2 seconds");
-        })?
+        Job::new_async("1/2 * * * * *", |_uuid, _l| Box::pin(work()))?
     ).await?;
 
 
@@ -22,4 +41,13 @@ async fn main() -> Result<(), JobSchedulerError> {
 
 
     Ok(())
+}
+
+async fn work() {
+
+
+    info!("I run every 2 seconds");
+
+    PROM_TX_SENT.set(80);
+
 }
---
+++ benchrunner-service/src/prometheus_sync.rs
@@ -0,0 +1,56 @@
+use std::time::Duration;
+
+use log::error;
+use prometheus::{Encoder, TextEncoder};
+use tokio::{
+    io::AsyncWriteExt,
+    net::{TcpListener, TcpStream, ToSocketAddrs},
+};
+
+pub struct PrometheusSync;
+
+impl PrometheusSync {
+    fn create_response(payload: &str) -> String {
+        format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            payload.len(),
+            payload
+        )
+    }
+
+    async fn handle_stream(stream: &mut TcpStream) -> anyhow::Result<()> {
+        let mut metrics_buffer = Vec::new();
+        let encoder = TextEncoder::new();
+
+        let metric_families = prometheus::gather();
+        encoder
+            .encode(&metric_families, &mut metrics_buffer)
+            .unwrap();
+
+        let metrics_buffer = String::from_utf8(metrics_buffer).unwrap();
+        let response = Self::create_response(&metrics_buffer);
+
+        stream.writable().await?;
+        stream.write_all(response.as_bytes()).await?;
+
+        stream.flush().await?;
+
+        Ok(())
+    }
+
+    pub fn sync(addr: impl ToSocketAddrs + Send + 'static) -> tokio::task::JoinHandle<anyhow::Result<()>> {
+        tokio::spawn(async move {
+            let listener = TcpListener::bind(addr).await?;
+
+            loop {
+                let Ok((mut stream, _addr)) = listener.accept().await else {
+                    error!("Error accepting prometheus stream");
+                    tokio::time::sleep(Duration::from_millis(1)).await;
+                    continue;
+                };
+
+                let _ = Self::handle_stream(&mut stream).await;
+            }
+        })
+    }
+}
