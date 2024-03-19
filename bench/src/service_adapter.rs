5ac6583361d54cb8845343c593b95dcc05be44e0
GroovieGermanikus <groovie@mango.markets>
2024-03-19 15:30:23 +0100

    played with websocket

--- bench/src/service_adapter.rs
+++ bench/src/service_adapter.rs
@@ -4,18 +4,25 @@ use crate::metrics::{Metric, TxMetricData};
 use crate::oldbench;
 use crate::oldbench::TransactionSize;
 use crate::tx_size::TxSize;
-use log::debug;
+use log::{debug, error, info};
 use solana_rpc_client::nonblocking::rpc_client::RpcClient;
 use solana_sdk::commitment_config::CommitmentConfig;
 use solana_sdk::hash::Hash;
 use solana_sdk::signature::Keypair;
 use solana_sdk::signer::Signer;
 use std::fmt::Display;
+use std::str::FromStr;
 use std::sync::atomic::AtomicU64;
 use std::sync::Arc;
 use std::time::Duration;
+use serde_json::{json, Value};
+use solana_rpc_client_api::response::RpcBlockUpdate;
+use solana_sdk::clock::Slot;
+use tokio::sync::broadcast::error::RecvError;
 use tokio::sync::RwLock;
 use tokio::time::Instant;
+use websocket_tungstenite_retry::websocket_stable::{StableWebSocket, WsMessage};
+use url::Url;
 
 #[derive(Debug, Clone)]
 pub struct BenchConfig {
@@ -48,36 +55,65 @@ pub async fn bench_servicerunner(
         rpc_addr.clone(),
         CommitmentConfig::confirmed(),
     ));
-    let bh = rpc_client.get_latest_blockhash().await.unwrap();
-    let slot = rpc_client.get_slot().await.unwrap();
-    let block_hash: Arc<RwLock<Hash>> = Arc::new(RwLock::new(bh));
-    let current_slot = Arc::new(AtomicU64::new(slot));
-    {
-        // block hash updater task
-        let block_hash = block_hash.clone();
-        let rpc_client = rpc_client.clone();
-        let current_slot = current_slot.clone();
-        tokio::spawn(async move {
-            loop {
-                let bh = rpc_client.get_latest_blockhash().await;
-                match bh {
-                    Ok(bh) => {
-                        let mut lock = block_hash.write().await;
-                        *lock = bh;
-                    }
-                    Err(e) => println!("blockhash update error {}", e),
-                }
 
-                let slot = rpc_client.get_slot().await;
-                match slot {
-                    Ok(slot) => {
-                        current_slot.store(slot, std::sync::atomic::Ordering::Relaxed);
-                    }
-                    Err(e) => println!("slot {}", e),
+    // let bh = rpc_client.get_latest_blockhash().await.unwrap();
+    // let slot = rpc_client.get_slot().await.unwrap();
+    let block_hash: Arc<RwLock<Hash>> = Arc::new(RwLock::new(Hash::new_unique()));
+    let current_slot = Arc::new(AtomicU64::new(0 as Slot));
+    {
+        let slot_subscribe =
+            json!({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "slotSubscribe",
+        });
+
+        let block_subscribe =
+            json!({
+            "jsonrpc": "2.0",
+            "id": 1,
+            "method": "blockSubscribe",
+            "params": [
+                "all",
+                {
+                      "commitment": "confirmed",
+                      "transactionDetails": "none"
+                 }
+            ]
+        });
+
+        let ws_addr = rpc_addr.replace("http", "ws").replace("https", "wss");
+        info!("Connecting to {}", ws_addr);
+        let mut wss = StableWebSocket::new_with_timeout(
+            Url::parse(ws_addr.as_str()).unwrap(),
+            block_subscribe,
+            Duration::from_secs(3),
+        )
+            .await
+            .unwrap();
+
+        let mut channel = wss.subscribe_message_channel();
+        loop {
+            match channel.recv().await {
+                Ok(WsMessage::Text(json_update)) => {
+                    let update: RpcBlockUpdate = serde_json::from_str(&json_update).unwrap();
+                    current_slot.store(update.slot, std::sync::atomic::Ordering::Relaxed);
+                    let mut lock = block_hash.write().await;
+                    *lock = Hash::from_str(&update.block.unwrap().blockhash).unwrap();
                 }
-                tokio::time::sleep(Duration::from_millis(100)).await;
+                Ok(WsMessage::Binary(_)) => {
+                    // should not happen
+                }
+                Err(RecvError::Lagged(_)) => {
+                    // should not happen
+                }
+                Err(RecvError::Closed) => {
+                    error!("websocket channel for blockSubscribe closed - aborting");
+                    break;
+                }
             }
-        })
+        }
+
     };
 
     {
