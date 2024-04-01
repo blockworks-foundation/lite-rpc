dffd66662dd8d962894b26e0494a00c4b7f807a5
Lou-Kamades <lou_kamades@proton.me>
2024-03-31 19:25:58 -0600

    temp: mem profiling

--- services-integration-test/tests/txn_broadcast.rs
+++ services-integration-test/tests/txn_broadcast.rs
@@ -6,10 +6,12 @@ use solana_sdk::{
 
 use log::{debug, info, trace};
 
-use std::time::Duration;
+use std::{thread, time::{self, Duration}};
 use tokio::time::Instant;
 
 use crate::setup::setup_tx_service;
+
+use jemalloc_ctl::{epoch, stats};
 
 mod setup;
 
@@ -24,6 +26,30 @@ const SAMPLE_SIZE: usize = 10000;
 pub async fn txn_broadcast() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
     info!("START BENCHMARK: txn_broadcast");
+
+    tokio::spawn(async move {
+
+        let mut last_allocated = stats::allocated::read().unwrap();
+        let mut last_resident = stats::resident::read().unwrap();
+        let mut last_mapped = stats::mapped::read().unwrap();
+        let mut last_active = stats::active::read().unwrap();
+
+        loop {
+            thread::sleep(time::Duration::from_secs(10));
+            // Retrieve memory statistics
+            // let stats = stats::active::mib().unwrap().read().unwrap();
+
+            let allocated = stats::allocated::read().unwrap();
+            let resident = stats::resident::read().unwrap();
+            let mapped = stats::mapped::read().unwrap();
+            let active = stats::active::read().unwrap();
+
+            info!("Current allocated memory: {} bytes -- diff {}", allocated, last_allocated as i64 - allocated as i64);
+            info!("Current resident memory: {} bytes -- diff {}", resident, last_resident as i64 - resident as i64);
+            info!("Current mapped memory: {} bytes -- diff {}", mapped, last_mapped as i64 - mapped as i64);
+            info!("Current active memory: {} bytes -- diff {}", active, last_active as i64 - active as i64);
+        }
+    });
 
     debug!("spawning tx_service");
     let (transaction_service, data_cache, _tx_jh) = setup_tx_service().await?;
@@ -42,7 +68,7 @@ pub async fn txn_broadcast() -> anyhow::Result<()> {
 
     // TODO: save stats
     // TODO: txn sink?
-    while i < SAMPLE_SIZE {
+    loop {
         let blockhash = data_cache.block_information_store.get_latest_blockhash(CommitmentConfig::confirmed()).await;
         let tx = create_memo_tx(&payer, blockhash, &mut rng, &params);
         let serialized = bincode::serialize::<VersionedTransaction>(&tx)
