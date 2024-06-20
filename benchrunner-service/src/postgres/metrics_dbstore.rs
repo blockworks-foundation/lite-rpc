a2dee849bd9828ba98a6a865dd476f81ef03f3b1
GroovieGermanikus <groovie@mango.markets>
2024-06-20 14:30:07 +0200

    rename column average_slot_confirmation_time_ms

--- benchrunner-service/src/postgres/metrics_dbstore.rs
+++ benchrunner-service/src/postgres/metrics_dbstore.rs
@@ -134,7 +134,7 @@ impl BenchMetricsPostgresSaver for BenchRunnerConfirmationRateImpl {
                 txs_confirmed,
                 txs_un_confirmed,
                 average_confirmation_time_ms,
-                average_slot_confirmation_time_ms,
+                average_slot_confirmation_time,
                 metric_json
              )
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
