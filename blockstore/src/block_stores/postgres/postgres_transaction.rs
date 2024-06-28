923ae71e70c21c88dfcd5000092539738e9668ca
GroovieGermanikus <groovie@mango.markets>
2024-06-28 14:14:22 +0200

    adjust autovacuum

--- blockstore/src/block_stores/postgres/postgres_transaction.rs
+++ blockstore/src/block_stores/postgres/postgres_transaction.rs
@@ -179,11 +179,11 @@ impl PostgresTransaction {
                 ALTER TABLE {schema}.transaction_ids
                     SET (
                         autovacuum_vacuum_scale_factor=0,
-                        autovacuum_vacuum_threshold=1000,
+                        autovacuum_vacuum_threshold=10000,
                         autovacuum_vacuum_insert_scale_factor=0,
-                        autovacuum_vacuum_insert_threshold=1000,
+                        autovacuum_vacuum_insert_threshold=50000,
                         autovacuum_analyze_scale_factor=0,
-                        autovacuum_analyze_threshold=1000
+                        autovacuum_analyze_threshold=50000
                         );
 
                 -- parameter 'schema' is something like 'rpc2a_epoch_592'
