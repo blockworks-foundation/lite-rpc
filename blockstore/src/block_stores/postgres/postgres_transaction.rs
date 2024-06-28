ae3379186cf8536799064f01d927b52bf2bca206
GroovieGermanikus <groovie@mango.markets>
2024-06-28 13:56:43 +0200

    transaction id table: apply include trick

--- blockstore/src/block_stores/postgres/postgres_transaction.rs
+++ blockstore/src/block_stores/postgres/postgres_transaction.rs
@@ -170,20 +170,20 @@ impl PostgresTransaction {
                 -- lookup table; maps signatures to generated int8 transaction ids
                 -- no updates or deletes, only INSERTs
                 CREATE TABLE {schema}.transaction_ids(
-                    transaction_id bigserial PRIMARY KEY WITH (FILLFACTOR=90),
+                    transaction_id bigserial NOT NULL,
                     signature varchar(88) NOT NULL,
-                    UNIQUE(signature) WITH (FILLFACTOR=80)
-                ) WITH (FILLFACTOR=100);
-                -- never put sig on TOAST
-                ALTER TABLE {schema}.transaction_ids ALTER COLUMN signature SET STORAGE PLAIN;
+                    PRIMARY KEY (transaction_id) INCLUDE(signature) WITH (FILLFACTOR=80),
+	                UNIQUE(signature) INCLUDE (transaction_id) WITH (FILLFACTOR=80)
+                ) WITH (FILLFACTOR=100, toast_tuple_target=128);
+                -- signature might end up on TOAST which is okey because the data gets pulled from index
                 ALTER TABLE {schema}.transaction_ids
                     SET (
-                        autovacuum_vacuum_scale_factor=0.2,
-                        autovacuum_vacuum_threshold=10000,
-                        autovacuum_vacuum_insert_scale_factor=0.2,
-                        autovacuum_vacuum_insert_threshold=50000,
-                        autovacuum_analyze_scale_factor=0.2,
-                        autovacuum_analyze_threshold=50000
+                        autovacuum_vacuum_scale_factor=0,
+                        autovacuum_vacuum_threshold=1000,
+                        autovacuum_vacuum_insert_scale_factor=0,
+                        autovacuum_vacuum_insert_threshold=1000,
+                        autovacuum_analyze_scale_factor=0,
+                        autovacuum_analyze_threshold=1000
                         );
 
                 -- parameter 'schema' is something like 'rpc2a_epoch_592'
