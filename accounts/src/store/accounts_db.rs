852a21ef561e9c8b7058812603f7b2c81ac97776
mango-dee <deedee1337@proton.me>
2024-11-27 12:58:23 +0800

    WIP to seek feedback

--- accounts/src/store/accounts_db.rs
+++ accounts/src/store/accounts_db.rs
@@ -1,23 +1,27 @@
-use std::cell::RefCell;
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 
 use async_trait::async_trait;
+use itertools::Itertools;
 use solana_accounts_db::accounts::Accounts;
 use solana_accounts_db::accounts_db::{AccountsDb as SolanaAccountsDb, AccountsDbConfig, AccountShrinkThreshold, CreateAncientStorage};
 use solana_accounts_db::accounts_file::StorageAccess;
-use solana_accounts_db::accounts_index::{AccountSecondaryIndexes, AccountsIndexConfig, IndexLimitMb};
+use solana_accounts_db::accounts_index::{AccountSecondaryIndexes, AccountsIndexConfig, IndexLimitMb, ScanConfig, ScanResult};
 use solana_accounts_db::ancestors::Ancestors;
 use solana_accounts_db::partitioned_rewards::TestPartitionedEpochRewards;
 use solana_rpc_client_api::filter::RpcFilterType;
 use solana_sdk::account::{Account, AccountSharedData, ReadableAccount};
-use solana_sdk::clock::Slot;
+use solana_sdk::clock::{BankId, Slot};
 use solana_sdk::genesis_config::ClusterType;
 use solana_sdk::pubkey::Pubkey;
+use solana_sdk::transaction_context::TransactionAccount;
 use task::spawn_blocking;
+use tokio::sync::RwLock;
 use tokio::task;
 
+use Commitment::{Confirmed, Finalized};
 use solana_lite_rpc_core::commitment_utils::Commitment;
+use solana_lite_rpc_core::commitment_utils::Commitment::Processed;
 use solana_lite_rpc_core::structures::account_data::AccountData;
 
 use crate::account_store_interface::{AccountLoadingError, AccountStorageInterface};
@@ -55,7 +59,7 @@ pub const ACCOUNTS_DB_CONFIG: AccountsDbConfig = AccountsDbConfig {
 pub struct AccountsDb {
     accounts: Accounts,
     // FIXME probably RwLock or similar
-    commitments: Mutex<RefCell<HashMap<Commitment, Slot>>>,
+    commitments: RwLock<HashMap<Commitment, Slot>>,
 }
 
 impl AccountsDb {
@@ -73,7 +77,7 @@ impl AccountsDb {
         let accounts = Accounts::new(Arc::new(db));
         Self {
             accounts,
-            commitments: Mutex::new(RefCell::new(HashMap::new())),
+            commitments: RwLock::new(HashMap::new()),
         }
     }
 
@@ -82,48 +86,33 @@ impl AccountsDb {
         let accounts = Accounts::new(Arc::new(db));
         Self {
             accounts,
-            commitments: Mutex::new(RefCell::new(HashMap::new())),
+            commitments: RwLock::new(HashMap::new()),
         }
     }
 }
 
 #[async_trait]
 impl AccountStorageInterface for AccountsDb {
-    // Why is there an update_account with commitment arg?
     async fn update_account(&self, account_data: AccountData, _commitment: Commitment) -> bool {
-        let shared_data = account_data.account.to_account_shared_data();
-        let account_to_store = [(&account_data.pubkey, &shared_data)];
-        self.accounts.store_accounts_cached((account_data.updated_slot, account_to_store.as_slice()));
-
-        println!("update {}", account_data.pubkey);
-
-        false
+        self.initilize_or_update_account(account_data).await;
+        true
     }
 
-    // FIXME this is upsert ?!
     async fn initilize_or_update_account(&self, account_data: AccountData) {
-        // let shared_data = account_data.account.to_account_shared_data();
-        // let account_to_store = [(&account_data.pubkey, &shared_data)];
-        // self.accounts.store_accounts_cached((account_data.updated_slot, account_to_store.as_slice()));
-
-        println!("init or update {}", account_data.pubkey);
-
         let shared_data = account_data.account.to_account_shared_data();
         let account_to_store = [(&account_data.pubkey, &shared_data)];
         self.accounts.store_accounts_cached((account_data.updated_slot, account_to_store.as_slice()));
-
-        // FIXME do we need this
     }
 
     async fn get_account(&self, account_pk: Pubkey, commitment: Commitment) -> Result<Option<AccountData>, AccountLoadingError> {
-        let ancestors = self.get_ancestors_from_commitment(commitment);
+        let ancestors = self.get_ancestors_from_commitment(commitment).await;
 
         let accounts_db = self.accounts.accounts_db.clone();
         Ok(
             spawn_blocking(move || {
                 accounts_db
                     .load_with_fixed_root(&ancestors, &account_pk)
-                    .map(|(shared_data, slot)| Self::to_account_data(account_pk, slot, shared_data))
+                    .map(|(shared_data, slot)| Self::convert_to_account_data(account_pk, slot, shared_data))
             })
                 .await
                 .map_err(|e| AccountLoadingError::FailedToSpawnTask(format!("Failed to spawn task: {:?}", e)))?
@@ -131,38 +120,136 @@ impl AccountStorageInterface for AccountsDb {
     }
 
     async fn get_program_accounts(&self, program_pubkey: Pubkey, account_filter: Option<Vec<RpcFilterType>>, commitment: Commitment) -> Option<Vec<AccountData>> {
-        let ancestors = self.get_ancestors_from_commitment(commitment);
+        let slot = self.get_slot_from_commitment(commitment).await;
+        let ancestors = Ancestors::from(vec![slot]);
+        let accounts_db = self.accounts.accounts_db.clone();
+        let scan_config = ScanConfig::new(true);
 
-        // self.accounts.load_by_program()
+        let transaction_accounts: ScanResult<Vec<TransactionAccount>> = spawn_blocking(move || {
+            let filter = move |data: &AccountSharedData| {
+                if data.owner() != &program_pubkey {
+                    return false;
+                }
+                match &account_filter {
+                    Some(filters) => {
+                        filters.iter().all(|filter| {
+                            match filter {
+                                RpcFilterType::DataSize(size) => data.data().len() == *size as usize,
+                                RpcFilterType::Memcmp(cmp) => cmp.bytes_match(data.data()),
+                                RpcFilterType::TokenAccountState => unimplemented!() // FIXME
+                            }
+                        })
+                    }
+                    None => true
+                }
+            };
 
-        todo!()
+            // implementation from solana accounts.rs with slight adjustments - load_by_program_with_filter
+            let mut collector = Vec::new();
+            accounts_db
+                .scan_accounts(
+                    &ancestors,
+                    BankId::from(slot),
+                    |some_account_tuple| {
+                        Self::load_while_filtering(&mut collector, some_account_tuple, |account| {
+                            filter(account)
+                        })
+                    },
+                    &scan_config,
+                )
+                .map(|_| collector)
+            // TODO The interface should be adjusted and return Rest<Vec<AccountData>> instead
+        }).await.unwrap_or(Ok(vec![]));
+
+        Some(
+            transaction_accounts.unwrap()
+                .into_iter()
+                .map(|ta| { Self::convert_to_account_data(ta.0, slot, ta.1) })
+                .collect_vec()
+        )
     }
 
+
     async fn process_slot_data(&self, slot: Slot, commitment: Commitment) -> Vec<AccountData> {
-        if commitment == Commitment::Finalized {
+        if commitment == Finalized {
             self.accounts.add_root(slot);
         }
-        println!("{} - {:?}", slot, commitment);
 
-        //FIXME ensure Finalized <= Confirmed <= Processed
-        // update all if this assumption is broken
-        // make sure is monotonic increasing
-        // self.commitments.insert(commitment, slot);
+        let commitments = self.commitments.read().await.clone();
+
+        let processed = commitments.get(&Processed).cloned().unwrap_or(Slot::from(0u64));
+        let confirmed = commitments.get(&Confirmed).cloned().unwrap_or(Slot::from(0u64));
+        let finalized = commitments.get(&Finalized).cloned().unwrap_or(Slot::from(0u64));
+
+        match commitment {
+            Processed => {
+                if slot > processed {
+                    self.commitments.write().await.insert(Processed, slot);
+                }
+            }
+            Confirmed => {
+                if slot > processed {
+                    self.commitments.write().await.insert(Processed, slot);
+                }
+                if slot > confirmed {
+                    self.commitments.write().await.insert(Confirmed, slot);
+                }
+            }
+            Finalized => {
+                if slot > processed {
+                    self.commitments.write().await.insert(Processed, slot);
+                }
+
+                if slot > confirmed {
+                    self.commitments.write().await.insert(Confirmed, slot);
+                }
+
+                if slot > finalized {
+                    self.commitments.write().await.insert(Finalized, slot);
+                }
+            }
+        }
 
         // FIXME do we need to return data from here? - why
-        self.commitments.lock().unwrap().borrow_mut().insert(commitment, slot);
+        // self.commitments.write().await.insert(commitment, slot);
         vec![]
     }
 }
 
 impl AccountsDb {
-    fn get_ancestors_from_commitment(&self, commitment: Commitment) -> Ancestors {
-        let lock = self.commitments.lock().unwrap();
-        let slot = lock.borrow().get(&commitment).unwrap().clone();
+    // borrowed from solana accounts.rs
+    fn load_while_filtering<F: Fn(&AccountSharedData) -> bool>(
+        collector: &mut Vec<TransactionAccount>,
+        some_account_tuple: Option<(&Pubkey, AccountSharedData, Slot)>,
+        filter: F,
+    ) {
+        if let Some(mapped_account_tuple) = some_account_tuple
+            .filter(|(_, account, _)| Self::is_loadable(account.lamports()) && filter(account))
+            .map(|(pubkey, account, _slot)| (*pubkey, account))
+        {
+            collector.push(mapped_account_tuple)
+        }
+    }
+
+    // borrowed from solana accounts.rs
+    fn is_loadable(lamports: u64) -> bool {
+        // Don't ever load zero lamport accounts into runtime because
+        // the existence of zero-lamport accounts are never deterministic!!
+        lamports > 0
+    }
+
+    async fn get_slot_from_commitment(&self, commitment: Commitment) -> Slot {
+        let lock = self.commitments.read().await;
+        let result = lock.get(&commitment).unwrap().clone();
+        result
+    }
+
+    async fn get_ancestors_from_commitment(&self, commitment: Commitment) -> Ancestors {
+        let slot = self.get_slot_from_commitment(commitment).await;
         Ancestors::from(vec![slot])
     }
 
-    fn to_account_data(pk: Pubkey, slot: Slot, shared_data: AccountSharedData) -> AccountData {
+    fn convert_to_account_data(pk: Pubkey, slot: Slot, shared_data: AccountSharedData) -> AccountData {
         AccountData {
             pubkey: pk,
             account: Arc::new(Account {
@@ -183,7 +270,7 @@ mod tests {
 
     use solana_sdk::pubkey::Pubkey;
 
-    use solana_lite_rpc_core::commitment_utils::Commitment;
+    use solana_lite_rpc_core::commitment_utils::Commitment::Confirmed;
 
     use crate::account_store_interface::AccountStorageInterface;
     use crate::store::accounts_db::create_account_data;
@@ -199,7 +286,7 @@ mod tests {
         let account_1_data = create_account_data(1, account_1_key, program_key, &[0u8; 23]);
         test_instance.initilize_or_update_account(account_1_data).await;
 
-        let result = test_instance.get_account(account_1_key, Commitment::Confirmed).await;
+        let result = test_instance.get_account(account_1_key, Confirmed).await;
     }
 }
 
